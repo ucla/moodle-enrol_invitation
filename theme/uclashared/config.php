b505573e6e2b58c1529fe08bb07eebecc24b2ca7
yangmungi <yangmungi@collab.sscnet.ucla.edu>
2011-03-16 16:14:27 -0700

    Adding javascript definition to config.php. Adding the Javascript file for the dropdown sublogo thing. Adding a bunch of language file strings for the sake of the footer links, control panel, sublogo. Added commentary to the layout file, as well as the logo, the sublogo text and dropdown, control panel, weeks display and footer links. Stole UCLA logo from CCLE PROD. Added a separator function and the local variable maintaining the name of the theme in the renderer, as well as the help and feedback link, the weeks display, the sublogo functions, the control panel renderer, the footer links and the control panel info. Added a whole bunch of bloody settings (footer links, the sublogo text and sublogo dropdown). Changed the CSS to sort of resemble our final goal, actually stolen from base/styles/core.css.

--- theme/uclashared/config.php
+++ theme/uclashared/config.php
@@ -16,17 +16,12 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Configuration for Moodle's standard theme.
- *
- * This theme is the default theme within Moodle 2.0, it builds upon the base theme
- * adding only CSS to create the simple look and feel Moodlers have come to recognise.
+ * Configuration for UCLA's Shared Server theme.
  *
  * For full information about creating Moodle themes, see:
  *  http://docs.moodle.org/en/Development:Themes_2.0
  *
- * @package   moodlecore
- * @copyright 2010 Sam Hemelryk
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @copyright 2010 UC Regents
  */
 
 $THEME->name = 'uclashared';
@@ -71,7 +66,10 @@ $THEME->layouts = array(
         'file' => $tf_course,
         'regions' => array('side-pre', 'side-post'),
         'defaultregion' => 'side-post',
-        'options' => array('langmenu'=>true),
+        'options' => array(
+            'langmenu' => true,
+            'controlpanel' => true
+        )
     ),
     'coursecategory' => array(
         'file' => $tf_general,
@@ -90,9 +88,6 @@ $THEME->layouts = array(
         'file' => $tf_frontpage,
         'regions' => array('side-pre', 'side-post'),
         'defaultregion' => 'side-post',
-        'options' => array(
-            'nocontrolpanel' => TRUE
-        )
     ),
     // Server administration scripts.
     'admin' => array(
@@ -166,3 +161,9 @@ $THEME->layouts = array(
 );
 
 $THEME->rendererfactory = 'theme_overridden_renderer_factory';
+
+$dropdown_contents = trim(get_config('theme_' . $THEME->name, 'logo_sub_dropdown'));
+if ($dropdown_contents != NULL && $dropdown_contents != '') {
+    $THEME->javascripts[] = 'shared_server_dropdown'; 
+}
+
