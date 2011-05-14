e502dc996391c74fa9d0be357da110937cca7bc6
Yangmun Choi <yangmungi@ucla.edu>
2011-05-13 17:58:05 -0700

    CCLE-2616 - YANGMUN: Adding Registrar tables and stored procedure classes.

---
+++ admin/report/uclaregistrar/registrar_ccle_getclasses.class.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+require_once(dirname(__FILE__).'/registrar_query.class.php');
+
+class registrar_ccle_getclasses extends registrar_query {
+    function validate($new, $old) {
+        $new = array_change_key_case($new, CASE_LOWER);
+
+        $tests = array('srs', 'term');
+
+        foreach ($tests as $criteria) {
+            if (!isset($new[$criteria])
+                && $new[$criteria] != $old[$criteria]) {
+                
+                return false;
+            }
+        }
+
+        return (object) $new;
+    }
+
+    function remote_call_generate($args) {
+        return "EXECUTE ccle_getClasses '".implode("','", $args)."' ";
+    }
+}
