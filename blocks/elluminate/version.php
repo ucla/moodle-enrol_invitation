489ba0c680b93ce6ca040cacea59844f3ddc91cf
jkluk <jkluk@ucla.edu>
2011-06-27 11:42:15 -0700

    CCLE-2357 - Adding Elluminate Integration to elluminate branch

---
+++ blocks/elluminate/version.php
@@ -0,0 +1,10 @@
+<?php
+
+/////////////////////////////////////////////////////////////////////////////////
+///  Code fragment to define the version of elluminate
+///  This fragment is called by moodle_needs_upgrading() and /admin/index.php
+/////////////////////////////////////////////////////////////////////////////////
+
+$plugin->version  = 2011020200;  // The current module version (Date: YYYYMMDDXX)
+
+?>
