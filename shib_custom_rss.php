d738f1de5ae3d12bcd313706117eb012ccf2820f
Yangmun Choi <yangmungi@ucla.edu>
2011-07-27 15:11:49 -0700

    CCLE-2659 - Creating dynamically generated script for the custom Shibboleth feed.

--- shib_custom_rss.php
+++ shib_custom_rss.php
@@ -1,16 +1,25 @@
+<?php
+
+header('Content Type: application/rss+xml');
+
+include(dirname(__FILE__) . '/config.php');
+
+$wwwroot = $CFG->wwwroot . '/';
+
+?>
 <?xml version="1.0"?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
-    <title>Common Collaboration and Learning Environment</title>
-    <link>http://nick1.ccle.ucla.edu/</link>
+    <title>CCLE Testing Environment</title>
+    <link><?php echo $wwwroot ?></link>
     <description>The UCLA Common Collaboration and Learning Environment (CCLE) supports instruction and research within a common digital environment for faculty, students and staff.</description>
     <language>en-us</language>
 
     <item>
       <title>Login</title>
       <description>
-        &lt;a href="http://nick1.ccle.ucla.edu/login/index.php?loginguest=1"&gt;Guest Access&lt;/a&gt; - access to publicly viewable sites&lt;br&gt;
-        &lt;a href="https://nick1.ccle.ucla.edu/login/UCLAlogin.php"&gt;Special Case Login&lt;/a&gt; -  only if you have been assigned a special case ID&lt;br&gt;
+        &lt;a href="<?php echo $wwwroot ?>login/index.php?loginguest=1"&gt;Guest Access&lt;/a&gt; - access to publicly viewable sites&lt;br&gt;
+        &lt;a href="<?php echo $wwwroot ?>login/ucla_login.php"&gt;Special Case Login&lt;/a&gt; -  only if you have been assigned a special case ID&lt;br&gt;
         Getting an error after a successful login? Your Logon ID might not be &lt;a href="http://kb.ucla.edu/articles/moodle-needs-certain-shibboleth-attributes"&gt;working properly&lt;/a&gt;.
       </description>
     </item>
