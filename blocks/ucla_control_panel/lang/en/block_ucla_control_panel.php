997e40773c0d0b573751a727f4836b1074d5d400
Yangmun Choi <yangmungi@ucla.edu>
2011-11-10 13:37:23 -0800

    CCLE-2387 - Changing strings to sentence case.

--- blocks/ucla_control_panel/lang/en/block_ucla_control_panel.php
+++ blocks/ucla_control_panel/lang/en/block_ucla_control_panel.php
@@ -16,65 +16,65 @@ $string['dummy_pre'] = '';
 $string['dummy_post'] = '';
 
 /** Default category name **/
-$string['default'] = 'Common Functions';
+$string['default'] = 'Common functions';
 
-$string['unknowntag'] = 'Ungrouped Functions';
+$string['unknowntag'] = 'Ungrouped functions';
 
 /** Stuff from common **/
-$string['ucla_cp_mod_common'] = 'Most Commonly Used';
+$string['ucla_cp_mod_common'] = 'Most commonly used';
 
-$string['email_students'] = 'E-mail Students';
-$string['email_students_post'] = '(via Announcements Forum)';
+$string['email_students'] = 'E-mail students';
+$string['email_students_post'] = '(via Announcements forum)';
 
-$string['email_students_exception'] = 'E-mail Students';
+$string['email_students_exception'] = 'E-mail students';
 $string['email_students_exception_post'] = 'There was a problem detecting your announcements forum. This is most likely caused by the fact you are using an incompatible course format.';
 
-$string['email_students_hidden_pre'] = 'E-mail Students is disabled when Announcements Forum hidden.';
-$string['email_students_hidden'] = 'Make Announcements Forum Visible';
+$string['email_students_hidden_pre'] = 'E-mail students is disabled when the Announcements forum is hidden.';
+$string['email_students_hidden'] = 'Make Announcements forum visible';
 
-$string['edit_office_hours'] = 'Update Office Hours & Contact Info';
+$string['edit_office_hours'] = 'Update office hours & contact info';
 $string['edit_office_hours_post'] = 'Update your office hours, office location and phone number.';
 
-$string['modify_sections'] = 'Modify Course Menu Sections';
+$string['modify_sections'] = 'Modify course menu sections';
 $string['modify_sections_post'] = '';
 
-$string['turn_editing_on'] = 'Delete or Update Materials';
-$string['turn_editing_on_post'] = '(or Update, Hide, Make Public)';
+$string['turn_editing_on'] = 'Delete or update materials';
+$string['turn_editing_on_post'] = '(or update, hide, make public)';
 
 /** Stuff from other **/
-$string['ucla_cp_mod_other'] = 'Other Commands';
+$string['ucla_cp_mod_other'] = 'Other commands';
 
-$string['import_classweb'] = 'Import Clasweb Site';
+$string['import_classweb'] = 'Import ClasWeb site';
 $string['import_classweb_post'] = 'Import content from a previous course on ClassWeb to this site.';
 
-$string['import_moodle'] = 'Import Moodle Course Data';
+$string['import_moodle'] = 'Import Moodle course data';
 $string['import_moodle_post'] = 'Copy activities and content from one of your Moodle courses to another.';
 
 $string['create_tasite'] = 'Create TA-Sites';
-$string['create_tasite_post'] = 'Create Teaching Assistant Sites.';
+$string['create_tasite_post'] = 'Create teaching assistant sites.';
 
-$string['view_roster'] = 'View Participants';
+$string['view_roster'] = 'View participants';
 $string['view_roster_post'] = 'Lists most recent logins of all students, TAs, and instructors.';
 
-$string['edit_profile'] = 'Edit User Profile';
+$string['edit_profile'] = 'Edit user profile';
 $string['edit_profile_post'] = 'You can change your personal information, upload a picture, turn your email on or off, etc.';
 
 /** Stuff for advanced **/
-$string['more_advanced'] = 'Advanced Functions';
+$string['more_advanced'] = 'Advanced functions';
 
-$string['ucla_cp_mod_advanced'] = 'Advanced Functions';
+$string['ucla_cp_mod_advanced'] = 'Advanced functions';
 
-$string['assign_roles'] = 'Assign Roles for {$a->shortname}';
+$string['assign_roles'] = 'Assign roles for {$a->shortname}';
 $string['assign_roles_post'] = 'Use this to assign roles specifically for {$a->shortname}.';
 
-$string['assign_roles_master'] = 'Assign Roles for {$a->shortname} (current course)';
+$string['assign_roles_master'] = 'Assign roles for {$a->shortname} (current course)';
 $string['assign_roles_master_post'] = 'Use this to assign roles (change people to students, TAs or guests to grant or remove privileges) specifically for {$a->shortname}.';
 
 
-$string['backup_copy'] = 'Create Course Backup';
+$string['backup_copy'] = 'Create course backup';
 $string['backup_copy_post'] = 'Make a copy of your course for use on another Moodle site.';
 
-$string['backup_restore'] = 'Restore from Backup';
+$string['backup_restore'] = 'Restore from backup';
 $string['backup_restore_post'] = 'Restore a backed up course, either to the current course or to another course of which you are an administrator.';
 
 $string['course_files'] = 'Files';
@@ -83,7 +83,7 @@ $string['course_files_post'] = 'Access course files directory to add or delete f
 $string['course_grades'] = 'Grades';
 $string['course_grades_post'] = 'View your Moodle gradebook. Activate the advanced gradebook features to use grade weighting, letter grades, extra credit, etc. See the Gradebook Manual for more information.';
 
-$string['course_edit'] = 'Course Settings';
+$string['course_edit'] = 'Course settings';
 $string['course_edit_post'] = 'Edit the full name and description of the course or hide the course from students.';
 
 /** End of File **/
