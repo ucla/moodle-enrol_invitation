0042937a121d8e92df0799a5edf05bd07fb61e67
Yangmun Choi <yangmungi@ucla.edu>
2011-11-10 16:06:38 -0800

    CCLE-2380 - Fixed strange wording of string file.

--- blocks/ucla_rearrange/lang/en/block_ucla_rearrange.php
+++ blocks/ucla_rearrange/lang/en/block_ucla_rearrange.php
@@ -11,7 +11,7 @@ $string['allcollapse'] = 'Collapse all';
 $string['rearrange_sections'] = 'Section rearranger';
 
 $string['error_section_noold'] = 'Missing section translation for {$a}';
-$string['error_section_previous'] = 'Course does has section without id! Changes have been ignored. If this error keeps recurring, please contact us.';
+$string['error_section_previous'] = 'Course has a section without id! Changes have been ignored.';
 
 $string['error_module_consistency'] = 'There was a module inconsistency, none of the changes were saved in order to protect your data. Please go back and try again.';
 
