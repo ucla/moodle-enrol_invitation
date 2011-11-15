70f8e8dd88e6f1afa48b1a67612149f4dee48722
Rex Lorenzo <rex@oid.ucla.edu>
2011-11-15 11:00:51 -0800

    CCLE-2320 - ENROLLMENT - Role Mappings - Changed editingteacher to editinginstructor

--- local/ucla/rolemappings.php
+++ local/ucla/rolemappings.php
@@ -7,9 +7,9 @@
 // *SYSTEM* defaults
 $role['ta']['*SYSTEM*'] = 'ta_admin'; // 02 whenever there is also an 01
 $role['ta_instructor']['*SYSTEM*'] = 'ta_admin'; 
-$role['supervising_instructor']['*SYSTEM*'] = 'editingteacher'; 
-$role['student_instructor']['*SYSTEM*'] = 'editingteacher'; 
-$role['instructor']['*SYSTEM*'] = 'editingteacher'; // Always an 01
+$role['supervising_instructor']['*SYSTEM*'] = 'editinginstructor'; 
+$role['student_instructor']['*SYSTEM*'] = 'editinginstructor';
+$role['instructor']['*SYSTEM*'] = 'editinginstructor'; // Always an 01
 $role['waitlisted']['*SYSTEM*'] = 'student'; // Student trying to add course
 $role['enrolled']['*SYSTEM*'] = 'student'; // Student enrolled in the course
 
