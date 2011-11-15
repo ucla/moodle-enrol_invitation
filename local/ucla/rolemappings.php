d165b1ab60ce49d50fb6669590923c6db29f7dbe
Rex Lorenzo <rex@oid.ucla.edu>
2011-11-15 11:53:03 -0800

    CCLE-2320 - ENROLLMENT - Role Mappings - changed role mappings once again

--- local/ucla/rolemappings.php
+++ local/ucla/rolemappings.php
@@ -6,8 +6,8 @@
 
 // *SYSTEM* defaults
 $role['ta']['*SYSTEM*'] = 'ta_admin'; // 02 whenever there is also an 01
-$role['ta_instructor']['*SYSTEM*'] = 'ta_admin'; 
-$role['supervising_instructor']['*SYSTEM*'] = 'editinginstructor'; 
+$role['ta_instructor']['*SYSTEM*'] = 'ta_instructor'; 
+$role['supervising_instructor']['*SYSTEM*'] = 'supervising_instructor'; 
 $role['student_instructor']['*SYSTEM*'] = 'editinginstructor';
 $role['instructor']['*SYSTEM*'] = 'editinginstructor'; // Always an 01
 $role['waitlisted']['*SYSTEM*'] = 'student'; // Student trying to add course
