31ba9ba06c1c30c1518556cc4f10d713361f72dc
Rex Lorenzo <rex@oid.ucla.edu>
2011-11-22 13:19:51 -0800

    CCLE-2306 - HELP SYSTEM BLOCK - removed unneeded comment

--- blocks/ucla_help/block_ucla_help.php
+++ blocks/ucla_help/block_ucla_help.php
@@ -17,9 +17,6 @@ class block_ucla_help extends block_base {
         $this->title = get_string('pluginname', 'block_ucla_help');
     }
 
-    // The PHP tag and the curly bracket for the class definition
-    // will only be closed after there is another function added in the next section.
-
     public function get_content()
     {
         if ($this->content !== NULL) {
