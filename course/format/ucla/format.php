e2feb456fb262fffdd0544d08dbcb13c2956a613
Rex Lorenzo <rex@oid.ucla.edu>
2011-12-05 17:58:08 -0800

    CCLE-2281 - FORMATS - fixed problems with css name conflict for class used to hide section numbers

--- course/format/ucla/format.php
+++ course/format/ucla/format.php
@@ -71,8 +71,6 @@ $has_capability_viewhidden =
 $has_capability_update = has_capability('moodle/course:update', $context);
 $get_accesshide = get_accesshide(get_string('currenttopic', 'access'));
 
-debugging('$get_accesshide = ' . $get_accesshide);
-
 // Cache all these get_string(), because you know, they're cached already...
 $streditsummary   = get_string('editsummary');
 $stradd           = get_string('add');
@@ -313,7 +311,7 @@ while ($section <= $course->numsections) {
        
         //// (LEFT) State ////
         $sectiontext = html_writer::tag('div', $section, array(
-                'class' => 'hidden'
+                'class' => 'ucla_format_hidden_section_number'
             ));
         $left_side = html_writer::tag('div', $currenttext.$sectiontext, array(
                 'class' => 'left side'
