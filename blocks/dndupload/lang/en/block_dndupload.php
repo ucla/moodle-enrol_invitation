45bb0aa149e4e40473ae61af7831b58ac33126be
Rex Lorenzo <rex@oid.ucla.edu>
2011-12-09 13:15:56 -0800

    CCLE-2769 - Drag and drop file upload block on M2 - initial commit using latest master copy from github

---
+++ blocks/dndupload/lang/en/block_dndupload.php
@@ -0,0 +1,30 @@
+<?php
+
+// This file is part of the Drag and drop upload block for Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+$string['addhere'] = 'Add file(s) here';
+$string['addlinkhere'] = 'Add link here';
+$string['addwebpagehere'] = 'Add page here';
+$string['blockname'] = 'Drag and drop upload';
+$string['dndworking'] = 'Drag and drop files, text or links from your desktop onto course sections to upload them';
+$string['filetoolarge'] = 'is too large to upload';
+$string['nameforlink'] = 'What do you want to call this link?';
+$string['nameforwebpage'] = 'What do you want to call this page?';
+$string['noajax'] = 'AJAX course editing must be enabled for drag and drop upload to work';
+$string['nofilereader'] = 'Your browser does not support drag and drop upload of files';
+$string['nopermission'] = 'You do not have permission to add files to this course';
+$string['noscript'] = 'Javascript must be enabled for drag and drop upload to work';
+$string['pluginname'] = 'Drag and drop upload';
