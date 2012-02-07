76c1756d01392087c8512bd00b03fa07fd5ec349
Rex Lorenzo <rex@oid.ucla.edu>
2012-02-06 18:09:55 -0800

    CCLE-2662 - fixed bad require_once statements

--- local/rolesmigration/importroles.php
+++ local/rolesmigration/importroles.php
@@ -21,9 +21,9 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-require_once((__FILE__).'../../config.php');
+require_once(dirname(__FILE__).'/../../config.php');
 require_once($CFG->libdir.'/adminlib.php');
-require_once((__FILE__).'importroles_form.php');
+require_once(dirname(__FILE__).'/importroles_form.php');
 require_once('lib.php');
 
 // Site context
