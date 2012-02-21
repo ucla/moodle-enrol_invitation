0f7946b0e3fb2a723e4cea62fa6c44f1346b3df3
Yangmun Choi <yangmungi@ucla.edu>
2012-02-21 15:43:43 -0800

    CCLE-2274 - Fixed role mappings to valid values.

--- local/ucla/rolemappings.php
+++ local/ucla/rolemappings.php
@@ -10,8 +10,7 @@ $role['ta_instructor']['*SYSTEM*'] = 'ta_instructor';
 $role['supervising_instructor']['*SYSTEM*'] = 'supervising_instructor'; 
 $role['student_instructor']['*SYSTEM*'] = 'editinginstructor';
 $role['instructor']['*SYSTEM*'] = 'editinginstructor'; // Always an 01
-$role['waitlisted']['*SYSTEM*'] = 'student'; // Student trying to add course
-$role['enrolled']['*SYSTEM*'] = 'student'; // Student enrolled in the course
+$role['student']['*SYSTEM*'] = 'student'; // Student enrolled in the course
 
 // chemistry roles
 $role['ta']['CHEM'] = 'ta'; // 02 whenever there is also an 01
