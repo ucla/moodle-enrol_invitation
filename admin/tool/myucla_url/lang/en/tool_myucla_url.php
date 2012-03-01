97ad50b31af26f14c830890f942d8b19ad139c4c
Yangmun Choi <yangmungi@ucla.edu>
2012-03-01 11:43:29 -0800

    CCLE-2277 - Fixing lang file issues for the MyUCLA URL updater. The flag names were mismatching.

--- admin/tool/myucla_url/lang/en/tool_myucla_url.php
+++ admin/tool/myucla_url/lang/en/tool_myucla_url.php
@@ -3,8 +3,8 @@
 $string['pluginname'] = 'MyUCLA URL updater';
 
 // Human expanses for each flag
-$string['neverflag'] = 'URL is already set at MyUCLA';
-$string['nooverwrite'] = $string['neverflag'];
-$string['alwaysflag'] = 'Forced update';
+$string['neversend'] = 'URL is already set at MyUCLA';
+$string['nooverwrite'] = $string['neversend'];
+$string['alwayssend'] = 'Forced update';
 
 // EoF
