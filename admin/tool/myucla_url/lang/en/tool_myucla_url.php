cfab253ba88863e1b955950969d05cbe9fa617d7
Yangmun Choi <yangmungi@ucla.edu>
2012-03-01 16:54:23 -0800

    CCLE-2277 - Clarified strings for message displayed for the various flags for myucla url updater.

--- admin/tool/myucla_url/lang/en/tool_myucla_url.php
+++ admin/tool/myucla_url/lang/en/tool_myucla_url.php
@@ -3,8 +3,8 @@
 $string['pluginname'] = 'MyUCLA URL updater';
 
 // Human expanses for each flag
-$string['neversend'] = 'URL is already set at MyUCLA';
-$string['nooverwrite'] = $string['neversend'];
+$string['neversend'] = 'Not sending to MyUCLA';
+$string['nooverwrite'] = 'URL is already set at MyUCLA';
 $string['alwayssend'] = 'Forced update';
 
 // EoF
