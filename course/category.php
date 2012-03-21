cc7d70fe0372ae5b79ef7eb11c6da03918a51a61
Rex Lorenzo <rexlorenzo@gmail.com>
2012-03-21 10:02:44 -0700

    CCLE-2878 - Display course short name field in Add/Edit screen course list

--- course/category.php
+++ course/category.php
@@ -278,6 +278,10 @@
         echo '<form id="movecourses" action="category.php" method="post"><div>';
         echo '<input type="hidden" name="sesskey" value="'.sesskey().'" />';
         echo '<table border="0" cellspacing="2" cellpadding="4" class="generalbox boxaligncenter"><tr>';
+        
+        // START UCLA MOD: CCLE-2878 - Display course short name field in Add/Edit screen course list   
+        echo '<th class="header" scope="col">'.get_string('shortnamecourse').'</th>';
+        // END UCLA MOD: CCLE-2878
         echo '<th class="header" scope="col">'.$strcourses.'</th>';
         if ($editingon) {
             echo '<th class="header" scope="col">'.$stredit.'</th>';
@@ -314,6 +318,11 @@
 
             $linkcss = $acourse->visible ? '' : ' class="dimmed" ';
             echo '<tr>';
+            
+            // START UCLA MOD: CCLE-2878 - Display course short name field in Add/Edit screen course list   
+            echo '<td><a '.$linkcss.' href="view.php?id='.$acourse->id.'">'. format_string($acourse->shortname) .'</a></td>';
+            // END UCLA MOD: CCLE-2878            
+            
             $coursename = get_course_display_name_for_list($acourse);
             echo '<td><a '.$linkcss.' href="view.php?id='.$acourse->id.'">'. format_string($coursename) .'</a></td>';
             if ($editingon) {
