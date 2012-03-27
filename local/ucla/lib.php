ucla/moodle-enrol_invitation

commit 067e3df50004f6919c481f427796393d24580934

CCLE-2308 - Adding summer term check to local/ucla/lib.php. Adding registrar links to courses without urls.

local/ucla/lib.php
@@ -385,16 +385,14 @@ function ucla_format_name($name=null) {
  *  Populates the reg-class-info cron, the subject areas and the divisions.
  **/
 function local_ucla_cron() {
-    global $CFG;
+    global $DB, $CFG;
 
     // TODO Do a better job figuring this out
-    $terms = $CFG->currentterm;
+    $terms = array($CFG->currentterm);
 
     include_once($CFG->dirroot . '/local/ucla/cronlib.php');
     ucla_require_registrar();
 
-    $terms = array($terms);
-    
     // Customize these times...?
     $works = array('classinfo', 'subjectarea', 'division');
 
@@ -578,6 +576,14 @@ function ucla_validator($type, $value) {
     }
     
     return $result == 1; 
+}
+
+/**
+ *  Convenience function to check if a term is a summer term.
+ *  Use this instead of preg_match()ing yourself.
+ **/
+function is_summer_term($term) {
+    return ucla_validator('term', $term) && preg_match('/1$/', $term);
 }
 
 /**

admin/tool/uclacoursecreator/uclacoursecreator.class.php
@@ -2251,7 +2251,7 @@ class uclacoursecreator {
      *  @return boolean Is the term a summer term?
      **/
     function match_summer($term) {
-        return preg_match('/1$/', $term);
+        return is_summer_term($term);
     }
 }
 

blocks/ucla_browseby/block_ucla_browseby.php
@@ -87,6 +87,12 @@ class block_ucla_browseby extends block_list {
         }
 
         $this->termslist = array($CFG->currentterm);
+
+        if (get_config('block_ucla_browseby', 'syncallterms')) {
+            $this->termslist = $this->get_all_terms();
+
+            set_config('syncallterms', false, 'block_ucla_browseby');
+        }
     }
 
     function cron() {
@@ -105,9 +111,11 @@ class block_ucla_browseby extends block_list {
         ucla_require_registrar();
 
         if (empty($terms)) {
-            debugging('no terms specified for browseby cron');
+            echo 'no terms specified for browseby cron' . "\n";
             return true;
         }
+
+        echo "\n";
 
         list($sqlin, $params) = $DB->get_in_or_equal($terms);
         $where = 'term ' . $sqlin;
@@ -135,6 +143,9 @@ class block_ucla_browseby extends block_list {
         foreach ($records as $record) {
             $term = $record->term;
             $subjarea = $record->subjarea;
+
+            echo "Handling $term $subjarea \n";
+
             $thisreg = array('term' => $term, 
                 'subjarea' => $subjarea);
             $toreg = array($thisreg);
@@ -180,6 +191,20 @@ class block_ucla_browseby extends block_list {
         ucla_require_registrar();
 
         return registrar_query::run_registrar_query($q, $d, true);
+    }
+
+    protected function get_all_terms() {
+        global $DB;
+
+        $termobjs = $DB->get_records('ucla_request_classes', null, '',
+            'DISTINCT term');
+
+        $terms = array();
+        foreach ($termobjs as $termobj) {
+            $terms[] = $termobj->term;
+        }
+
+        return $terms;
     }
 }
 

blocks/ucla_browseby/handlers/collab.class.php
@@ -69,35 +69,24 @@ class collab_handler extends browseby_handler {
 
         $courselist = array();
         if (!empty($collab_cat->courses)) {
-            // Note that this is a csv of role ids and is designed to
-            // be accessed by the GUI
-            $roles_visible = get_config('block_ucla_browseby', 
-                'collab_roles');
+            // Default roles to use, get these shortname's role.id
+            $rolenames = self::get_default_roles_visible();
+            $allroles = get_all_roles();
 
-            // Figure out which roles we're going to display as project
-            // lead and coures creator
-            if ($roles_visible) {
-                $roleids = explode(',', $roles_visible);
-            } else {
-                // Default roles to use, get these shortname's role.id
-                $rolenames = self::get_default_roles_visible();
-                $allroles = get_all_roles();
+            $iroles = array();
+            foreach ($allroles as $role) {
+                $iroles[$role->shortname] = $role;
+            }
 
-                $iroles = array();
-                foreach ($allroles as $role) {
-                    $iroles[$role->shortname] = $role;
-                }
+            $roleids = array();
+            $rolefullnames = array();
+            foreach ($rolenames as $rolename) {
+                if (isset($iroles[$rolename])) {
+                    $role = $iroles[$rolename];
 
-                $roleids = array();
-                $rolefullnames = array();
-                foreach ($rolenames as $rolename) {
-                    if (isset($iroles[$rolename])) {
-                        $role = $iroles[$rolename];
-
-                        $rshortname = $role->shortname;
-                        $roleids[$rshortname] = $role->id;
-                        $rolefullnames[$rshortname] = $role->name;
-                    }
+                    $rshortname = $role->shortname;
+                    $roleids[$rshortname] = $role->id;
+                    $rolefullnames[$rshortname] = $role->name;
                 }
             }
 
@@ -179,8 +168,16 @@ class collab_handler extends browseby_handler {
             $table = new html_table();
             $table->data = $data;
 
+            $headers = array('sitename', 'projectlead', 'coursecreators');
+            $dispheaders = array();
+
+            foreach ($headers as $header) {
+                $dispheaders[] = get_string($header, 'block_ucla_browseby');
+            }
+
+            $table->head = $dispheaders;
+
             $list = html_writer::table($table);
-        
         } else {
             $title = get_string('collab_nocoursesincat', 
                 'block_ucla_browseby');

blocks/ucla_browseby/handlers/course.class.php
@@ -11,9 +11,12 @@ class course_handler extends browseby_handler {
         ubc.url,
         ubc.term,
         ubc.srs,
+        ubc.ses_grp_cd AS session_group,
+        ubc.session AS session_code,
         ubc.coursetitlelong AS course_title,
         ubc.sectiontitle AS section_title,
         ubc.sect_enrl_stat_cd AS enrolstat,
+        ubc.catlg_no AS sect_num,
         ubci.uid,
         COALESCE(user.firstname, ubci.firstname) AS firstname,
         COALESCE(user.lastname, ubci.lastname) AS lastname,
@@ -45,10 +48,16 @@ class course_handler extends browseby_handler {
         $fullcourselist = array();
 
         if (isset($args['term'])) {
+            $term = $args['term'];
             $termwhere = ' AND ubc.term = :term ';
             $param['term'] = $args['term'];
         } else {
             $termwhere = '';
+        }
+
+        $issummer = false;
+        if (is_summer_term($term)) {
+            $issummer = true;
         }
 
         if (isset($args['subjarea'])) {
@@ -141,28 +150,36 @@ class course_handler extends browseby_handler {
                     $this->fullname($course);
             } else {
                 $courseobj = new stdclass(); 
+                $coursetitle = ucla_make_course_title(get_object_vars($course));
+
                 if (get_config('block_ucla_browseby', 'use_local_courses')
                         && isset($course->courseid)) {
                     $course->id = $course->courseid;
                     $courseobj->url = 
                         uclacoursecreator::build_course_url($course);
                 } else {
-                    if (isset($course->url)) {
+                    if (!empty($course->url)) {
                         $courseobj->url = $course->url;
+                        $courseobj->dispname = $coursetitle;
                     } else {
-                        // Display registrar?
+                        $courseobj->url = $this->registrar_url(
+                            $course
+                        );
+
+                        $courseobj->nonlinkdispname = $coursetitle;
+                        $courseobj->dispname =  '(' . html_writer::tag(
+                            'span', get_string('registrar_link', 
+                                'block_ucla_browseby'),
+                            array('class' => 'registrar-link')) . ')';
                     }
                 }
 
-                $cancelledmess= '';
+                $cancelledmess = '';
                 if (enrolstat_is_cancelled($course->enrolstat)) {
                     $cancelledmess = html_writer::tag('span', 
                         get_string('cancelled'), 
                         array('class' => 'ucla-cancelled-course')) . ' ';
                 }
-
-                $courseobj->dispname = ucla_make_course_title(get_object_vars(
-                    $course));
 
                 // TODO make this function name less confusing
                 $courseobj->fullname = $cancelledmess . 
@@ -172,6 +189,8 @@ class course_handler extends browseby_handler {
 
                 $courseobj->instructors = 
                     array($course->uid => $this->fullname($course));
+
+                $courseobj->session_group = $course->session_group;
             }
 
             $fullcourseslist[$k] = $courseobj;
@@ -187,15 +206,62 @@ class course_handler extends browseby_handler {
             $course->instructors = $instrstr;
             $fullcourseslist[$k] = $course;
         }
-
-        $table = block_ucla_browseby_renderer::ucla_browseby_courses_list(
-            $fullcourseslist);
-
+        
         $s .= block_ucla_browseby_renderer::render_terms_selector(
             $args['term'], $terms_select_where, $terms_select_param);
 
-        $s .= html_writer::table($table);
-        
+        $headelements = array('course', 'instructors', 'coursedesc');
+        $headelementsdisp = array();
+
+        foreach ($headelements as $headelement) {
+            $headelementsdisp[] = get_string($headelement, 
+                'block_ucla_browseby');
+        }
+
+        if ($issummer) { 
+            $sessionsplits = array();
+            foreach ($fullcourseslist as $k => $fullcourse) {
+                $session = $fullcourse->session_group;
+
+                if (!isset($sessionsplits[$session])) {
+                    $sessionsplits[$session] = array();
+                }
+
+                unset($fullcourse->session_group);
+
+                $sessionsplits[$session][$k] = $fullcourse;
+            }
+
+            $table = new html_table();
+            
+            foreach ($sessionsplits as $session => $courses) {
+                $sessioncell = new html_table_cell();
+                $sessioncell->text = $OUTPUT->heading(get_string(
+                    'session_break', 'block_ucla_browseby', $session), 3);
+
+                $sessioncell->colspan = '3';
+                $sessionrow = new html_table_row();
+                $sessionrow->cells[] = $sessioncell;
+                
+                $subtable = block_ucla_browseby_renderer::
+                    ucla_browseby_courses_list($courses);
+
+                $table->data[] = $sessionrow;
+                $table->data = array_merge($table->data, $subtable->data);
+            }
+
+            $s .= html_writer::table($table);
+        } else {
+            foreach ($fullcourseslist as $k => $course) {
+                unset($fullcourseslist[$k]->session_group);
+            }
+
+            $table = block_ucla_browseby_renderer::ucla_browseby_courses_list(
+                $fullcourseslist);
+
+            $s .= html_writer::table($table);
+        }
+
         return array($t, $s);
     }
     
@@ -224,11 +290,28 @@ class course_handler extends browseby_handler {
 
         return $name;
     }
+
+    function registrar_url($course) {
+        $page = 'http://www.registrar.ucla.edu/schedule/detselect';
+
+        $term = $course->term;
+
+        $issummerterm = is_summer_term($term);
+        $query = '.aspx?termsel=' . $term . '&subareasel=' 
+            . urlencode($course->subj_area) . '&idxcrs=' 
+            . urlencode($course->sect_num);
+
+        if ($issummerterm) {
+            $page .= '_summer';
+            $query .= $course->session_group;
+        }
+
+        return $page . $query;
+    }
     
     protected function get_user($userid) {
         global $DB;
 
         return $DB->get_record('user', array('id' => $userid));
     }
-
 }

blocks/ucla_browseby/lang/en/block_ucla_browseby.php
@@ -1,34 +1,39 @@
 <?php
 
 $string['pluginname'] = 'UCLA browse-by';
-
 $string['displayname'] = 'Browse by:';
 
+// Links displayed in the block
 $string['link_subjarea'] = 'Subject area';
 $string['link_division'] = 'Division';
 $string['link_instructor'] = 'Instructor';
 $string['link_collab'] = 'Collaboration sites';
 $string['link_mycourses'] = 'My sites';
 
+// This is for errors
 $string['illegaltype'] = 'View for "{$a}" does not exist.';
 
-$string['noresults'] = 'No results.';
-
+// Subject area
 $string['subjarea_title'] = 'Subject areas in {$a}';
 $string['all_subjareas'] = 'All hosted subject areas'; 
+
+// Divisions
 $string['division_title'] = 'Divisions';
 $string['division_none'] = 'No divisions were found on this server';
 $string['division_noterm'] = 'No divisions were found for this term, please try another term.';
 
+// Instructors
 $string['instructorsall'] = 'All instructors';
 $string['instructorswith'] = 'Instructors, last name starting with "{$a}"';
 $string['instructosallterm'] = 'All instructors for {$a}';
 $string['noinstructorsterm'] = 'There are no instructors teaching on this server for {$a}, please try another term.';
 $string['noinstructors'] = 'There were no instructors found.';
 
+// Instructors -> courses
 $string['coursesbyinstr'] = 'Courses taught by {$a}';
 $string['coursesinsubjarea'] = 'Courses in "{$a}"';
 
+// Collaborations
 $string['collab_notfound'] = 'No collaboration sites found.';
 $string['collab_notcollab'] = 'This category is not considered a category for collaboration sites.';
 $string['collab_coursesincat'] = 'Courses in this category';
@@ -36,6 +41,11 @@ $string['collab_viewall'] = 'Collaboration sites';
 $string['collab_viewin'] = 'Collaboration sites in {$a}';
 $string['collab_nocoursesincat'] = 'No courses were found in this cateogry';
 
+$string['sitename'] = 'Site name';
+$string['projectlead'] = 'Project lead';
+$string['coursecreators'] = 'Course owner';
+
+// Options
 $string['title_division'] = 'Disable browse-by division';
 $string['title_subjarea'] = 'Disable browse-by subject areas';
 $string['title_instructor'] = 'Disable browse-by instructors';
@@ -46,4 +56,16 @@ $string['desc_subjarea'] = 'Check box to disable the ability to use subject area
 $string['desc_instructor'] = 'Check box to disable the ability to see the courses an instructor is teaching.';
 $string['desc_collab'] = 'Check box to disable the ability for guests to browse collaboration sites.';
 
+$string['title_syncallterms'] = 'Sync for all terms';
+$string['desc_syncallterms'] = 'Check box to enable synchronization of all terms that is available on this server. It will uncheck itself after it runs.';
+
+
+// Courses view
 $string['nousersinrole'] = 'N / A';
+$string['session_break'] = 'Summer session {$a}';
+$string['registrar_link'] = 'Registrar';
+
+// Headers in courses view
+$string['course'] = 'Course';
+$string['instructors'] = 'Instructors';
+$string['coursedesc'] = 'Course description';

blocks/ucla_browseby/renderer.php
@@ -24,7 +24,7 @@ class block_ucla_browseby_renderer {
         }
 
         if (!empty($lists)) {
-            $ringer = 0;
+            $ringer = count($lists) - 1;
             foreach ($lists as $list) {
                 $s .= html_writer::tag('ul', $list, array(
                     'class' => 'list' . $ringer . " $customclass"
@@ -32,7 +32,7 @@ class block_ucla_browseby_renderer {
             }
         }
 
-        return $s;
+        return html_writer::tag('div', $s, array('class' => 'browsebylist'));
     }
 
     static function ucla_custom_list_render_helper($data) {
@@ -64,7 +64,13 @@ class block_ucla_browseby_renderer {
         $data = array();
 
         foreach ($courses as $course) {
-            $courselink = html_writer::link(new moodle_url($course->url),
+            if (!empty($course->nonlinkdispname)) {
+                $courselink = $course->nonlinkdispname . ' ';
+            } else {
+                $courselink = '';
+            }
+
+            $courselink .= html_writer::link(new moodle_url($course->url),
                 $course->dispname);
 
             $data[] = array($courselink, $course->instructors, 
@@ -119,7 +125,7 @@ class block_ucla_browseby_renderer {
     static function terms_selector($defaultterm=false, 
             $restrictor_where=false, $restrictor_params=null) {
         global $DB, $PAGE;
-       
+
         if (!empty($restrictor_where)) {
             $terms = $DB->get_records_select('ucla_request_classes', 
                 $restrictor_where, $restrictor_params, '', 'DISTINCT term');

blocks/ucla_browseby/settings.php
@@ -15,4 +15,14 @@ if ($ADMIN->fulltree) {
             get_string('desc_' . $type, 'block_ucla_browseby'), 
             0));
     }
+
+    $settings->add(new admin_setting_configcheckbox(
+        'block_ucla_browseby/syncallterms',
+         get_string('title_syncallterms', 'block_ucla_browseby'),
+         get_string('desc_syncallterms', 'block_ucla_browseby'), 0));
+    
+    $settings->add(new admin_setting_configcheckbox(
+        'block_ucla_browseby/use_local_courses',
+         get_string('title_use_local_courses', 'block_ucla_browseby'),
+         get_string('desc_use_local_courses', 'block_ucla_browseby'), 0));
 }

commit 6ae2406d6dbbb3f39ce19a589aa08bfff82a9e5e

CCLE-2308 - Removing summer courses duplicate function caused by 067e3df50004f6919c481f427796393d24580934.

local/ucla/lib.php
@@ -579,14 +579,6 @@ function ucla_validator($type, $value) {
 }
 
 /**
- *  Convenience function to check if a term is a summer term.
- *  Use this instead of preg_match()ing yourself.
- **/
-function is_summer_term($term) {
-    return ucla_validator('term', $term) && preg_match('/1$/', $term);
-}
-
-/**
  * Given a pseudorole (from get_pseudorole), returns what moodle role a user
  * should be assigned for a given department. First a look-up is done in the 
  * database for a given pseudorole and subject area. Then the function looks