da371c4d031f778237c7f9c6f91a20b22bc0afa8
Yangmun Choi <yangmungi@ucla.edu>
2012-03-27 14:18:36 -0700

    CCLE-2308 - Adding events to trigger browseby synchronization job.

--- blocks/ucla_browseby/version.php
+++ blocks/ucla_browseby/version.php
@@ -2,7 +2,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2012031900;
+$plugin->version = 2012032700;
 $plugin->component = 'block_ucla_browseby';
 // Disable cron, but there is another interface for it
 $plugin->cron = 0;
