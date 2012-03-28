353d499828bc0c5cc7f393f3456daea86952314f
Matthew McKeen <mmckeen@oid.ucla.edu>
2012-03-28 16:19:17 -0700

    CCLE-2312 - Rebase under master after mistaken merge in previous libraryreserves branch

---
+++ blocks/ucla_library_reserves/lang/en/block_ucla_library_reserves.php
@@ -0,0 +1,8 @@
+<?php
+    $string['pluginname'] = 'Library reserves';
+    $string['title'] = 'Library reserves';
+
+    $string['displayname'] - 'Library Reserves';
+
+    $string['headerlibraryreservesurl'] = 'Source URL';
+    $string['desclibraryreservesurl'] = 'CSV source URL for Library Reserves data';
