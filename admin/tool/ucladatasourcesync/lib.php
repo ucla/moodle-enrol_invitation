76bda2d16dc55e13df52f5daf01f4bdd94f1a993
Matthew McKeen <mmckeen@oid.ucla.edu>
2012-03-29 12:58:56 -0700

    CCLE-2790 - Refactoring of plugin name for better code style

---
+++ admin/tool/ucladatasourcesync/lib.php
@@ -0,0 +1,158 @@
+<?php
+/**
+* Library for use by the Datasource Syncronization Scripts of the Bruincast (CCLE-2314), Library reserves (CCLE-2312), and Video furnace (CCLE-2311) blocks
+*
+* See CCLE-2790 for details.
+**/
+
+//satisfy moodle's requirement for cli scripts
+define('CLI_SCRIPT', true);
+
+require_once(dirname(dirname(dirname(dirname(__FILE__)))) . "/config.php");
+
+/**
+* Returns an array of raw CSV data from the CSV file at datasource_url.
+* @param $datasource_url The URL of the CSV data to attempt to retrieve.
+**/
+function get_csv_data($datasource_url){
+
+    $lines = array();
+    $fp = fopen($datasource_url, 'r');
+
+    if ($fp) {
+        $lines = '';
+
+        while (!feof($fp)) {
+            $lines[] = fgetcsv($fp);
+        }
+    }
+
+    if (empty($lines)) {
+        echo "\n... ERROR: Could not open $datasource_url!\n";
+        exit(5);
+    }
+
+    return $lines;
+}
+
+/**
+* Returns an array of cleaned and parsed CSV data from the unsafe and/or unclean input array of data.
+* @param $data The array of unsafe CSV data.
+* @param $table_name The moodle DB table name against which to validate the field labels of the CSV data.
+* @note Currently only works with the Bruincast update script at ./bruincast_dbsync.php  May cause undefined behaviour if used with other datasets.
+**/
+function cleanup_csv_data($data_array, $table_name){
+    
+    // get global variables
+    global $CFG;
+    global $DB;
+
+    $incoming_data = &$data_array; // mmm... memory savings.....
+    $posfields = array();
+
+    // Automatically ignore fields
+    $curfields = $DB->get_records_sql("DESCRIBE {$CFG->prefix}".$table_name);
+
+    foreach ($curfields as $fieldname => $fielddata) {
+
+        // Skip the field `id`
+        if ($fieldname == 'id') {
+            continue;
+        }
+
+        $posfields[$fieldname] = $fieldname;
+    }
+
+    
+
+    // Assuming the field descriptor line is going to come before all the other lines
+    $field_descriptors_obtained = FALSE;
+    $fields_desc_line = -1;
+    $total_lines = count($incoming_data);
+
+    while (!$field_descriptors_obtained) {
+
+        $fields_desc_line++;
+
+        if ($fields_desc_line == $total_lines) {
+            die ("\n"."Could not find any lines that match any field in the DB!"."\n");
+        }
+
+        $file_fields = array();
+
+        foreach ($incoming_data[$fields_desc_line] as $tab_num => $field_name) {
+
+            if (!isset($posfields[$field_name])) {
+                $ignored_fields[] = $field_name;
+            } else {
+                $finfields[] = $field_name;
+            }
+    
+            $file_fields[$field_name] = $field_name;
+        }
+
+        // Assume that this is the line that we want
+        $field_descriptors_obtained = TRUE;
+
+        foreach ($posfields as $fieldname => $field) {
+        
+            if (!isset($file_fields[$field])) {
+                // This line is not the field descriptor line!
+                $field_descriptors_obtained = FALSE;
+            }
+
+        }
+    }
+
+    // Reindex the data for nicer formatting
+    $data_incoming = array();
+    $invalid_restrictions = array();
+    for ($line = $fields_desc_line + 1; $line < count($incoming_data); $line++) {
+
+        // Make sure this line has data as we have fields
+        if (count($incoming_data[$line]) != count($file_fields)) {
+            echo "Line $line is badly formed!\n";
+            continue;
+        }
+    
+        foreach ($incoming_data[$fields_desc_line] as $tab_num => $field_name) {
+
+            $data = $incoming_data[$line][$tab_num];
+            $field = trim($field_name);
+    
+            if (in_array($field, $ignored_fields)) {
+                continue;
+            }
+
+            // Pad the beginning with 0s
+            if ($field_name == 'srs') {
+                $data = sprintf('%09s', $data);
+            }
+
+            if ('restricted' == $field) {
+                // make sure that restricted is a known value
+                if (!in_array($data, array('Open', 'Restricted', 'See Instructor', 'Online'))) {
+                    $invalid_restrictions[] = "Line $line has unknown restriction of $data. Marking it as 'Undefined'. Please add it.\n";
+                    // restriction is not in lang file (for now). just set
+                    // restriction as undefined
+                    $data = 'Undefined';
+                }
+            }
+        
+            $data_incoming[$line][$field] = $data;
+        }
+    }
+
+    if (!empty($invalid_restrictions)) {
+        $invalid_restrictions = implode("\n", $invalid_restrictions);
+
+        if (!isset($CFG->bruincast_errornotify_email) || $CFG->quiet_mode) {
+            echo $invalid_restrictions;
+        } else {
+            mail($CFG->bruincast_errornotify_email, 'BruinCast Data Issues (' . date('r') . ')', $invalid_restrictions);
+        }
+    }
+
+    
+    return $data_incoming;
+}
---
+++ admin/tool/ucladatasourcesync/libraryreserves_dbsync.php
@@ -0,0 +1,132 @@
+<?php
+/**
+* Command line script to parse, verify, and update Library reserves entries in the Moodle database.
+*
+* $CFG->libraryreserves_data is defined in the plugin configuration at 
+* Site administration->Plugins->Blocks->Library reserves
+*
+* See CCLE-2312 for details
+**/
+
+require_once('lib.php');
+
+// Check to see that config variable is initialized
+if (!isset($CFG->libraryreserves_data))
+{
+    die("\n".get_string('errlrmsglocation','tool_ucladatasourcesync')."\n");
+}
+
+// Begin database update
+update_libraryreserves_db();
+
+function update_libraryreserves_db(){
+   // get global variables
+   global $CFG, $DB;
+  
+   echo get_string('lrstartnoti','tool_ucladatasourcesync');
+   
+   $datasource_url = $CFG->libraryreserves_data;
+
+   $incoming_data = array();
+
+   # read the file into a two-dimensional array
+   $lines = file($datasource_url);
+
+   foreach ($lines as $line_num => $line) {
+           # stop processing data if we hit the end of the file
+               if ($line != "=== EOF ===\n") {
+                           # remove the newline at the end of each line
+                                   $line = rtrim($line);
+                                        $incoming_data[$line_num] = explode("\t", $line);
+               }
+   }
+
+   
+   $fields = array();
+   
+   if($lines == FALSE)
+   {
+       die("\n".get_string('errlrfileopen','tool_ucladatasourcesync')."\n"); 
+   }
+
+   $curfields = $DB->get_records_sql("DESCRIBE {$CFG->prefix}"."ucla_library_reserves");
+   
+   foreach ($curfields as $fieldname => $fielddata) {
+       
+       // Skip the field 'id'
+       if ($fieldname == 'id') {
+           continue;
+       }
+
+       $fields[$fieldname] = $fieldname;
+   }
+   
+   $data_incoming = array();
+
+   // Check if all entries have the correct number of columns  
+
+   for ($row = 1; $row < sizeof($incoming_data); $row++) {
+       if (sizeof($incoming_data[$row]) != count($fields)) {
+            die("\n".get_string('errinvalidrowlen','tool_ucladatasourcesync')."\n");
+       }
+
+       // Bind field label to data
+
+       $tabnum = 0;
+
+       foreach ($fields as $tab_num => $field_name) {
+
+           $data = $incoming_data[$row][$tabnum];
+           $field = trim($field_name);
+           
+           if($fieldname == 'id') {
+               continue;
+           }
+            
+           if ($field_name == 'srs') {
+               $data = sprintf('%09s', $data);
+           }
+          
+           $data_incoming[$row][$field] = $data;
+           
+           $tabnum++; 
+       }
+   }
+  
+   $data = &$data_incoming;
+
+   // Drop table and refill with data
+   $DB->delete_records('ucla_library_reserves');
+
+   $insert_count = 0;
+   $line = 1;
+   $row = new stdClass();
+   $index = FALSE; 
+
+   for ($line; $line < count($data); $line++) {
+       
+       foreach ($data[$line] as $field => $fieldvalue) {
+           $row->$field = $fieldvalue;
+       }
+
+       try {
+           $index = $DB->insert_record('ucla_library_reserves', $row);
+       } catch(Exception $e) {
+           // Do nothing, to handle cases where rows are invalid beyond norms.  Does not insert row.
+       }
+    
+       if($index) {
+           $insert_count++;
+       }
+
+   }
+
+   if ($insert_count == 0) {
+       echo "\n".get_string('errbcinsert','tool_ucladatasourcesync')."\n";
+   } else {
+       echo "\n... ".$insert_count." ".get_string('lrsuccessnoti','tool_ucladatasourcesync')."\n";
+   }
+       
+}
+
+
