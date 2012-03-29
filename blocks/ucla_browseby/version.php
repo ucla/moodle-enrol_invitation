90fc5170c645cdb03c28262949bf41bda304045e
Yangmun Choi <yangmungi@ucla.edu>
2012-03-28 23:52:55 -0700

    CCLE-2308 - Fixing issues at fresh install.

--- blocks/ucla_browseby/version.php
+++ blocks/ucla_browseby/version.php
@@ -5,3 +5,4 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version = 2012032704;
 $plugin->component = 'block_ucla_browseby';
 $plugin->cron = (60 * 60 * 24);
+$plugin->dependencies = array('local_ucla' => 2012020100);
