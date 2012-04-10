84b6303686f6880e622b9f0ef75a1ebeae713c77
Nevin Li <nli@oid.ucla.edu>
2012-04-09 19:12:27 -0700

    CCLE-2307  - Refactored date functionsto use the built in PHP date functions.

--- blocks/ucla_weeksdisplay/block_ucla_weeksdisplay.php
+++ blocks/ucla_weeksdisplay/block_ucla_weeksdisplay.php
@@ -21,13 +21,9 @@ require_once($CFG->dirroot . '/local/ucla/lib.php');
 
 class block_ucla_weeksdisplay extends block_base {
     
-    /*
-     * Session code for a regular fall/winter/spring session.
-     */
+    /** Session code for a regular fall/winter/spring session. */
     const RG = 'RG'; 
-     /*
-     * Session codes for the various regular summer sessions.
-     */   
+     /** Session codes for the various regular summer sessions. */   
     const S1 = '6A'; 
     const S2 = '8A';
     const S3 = '6C';
@@ -44,7 +40,6 @@ class block_ucla_weeksdisplay extends block_base {
     }
     
     function cron(){
-        
         //Include registrar files.
         ucla_require_registrar();
         global $CFG;
@@ -64,13 +59,96 @@ class block_ucla_weeksdisplay extends block_base {
             $is_date_in_session = 
                     is_date_in_session($system_date,$regular_sessions[RG]);
             if($is_date_in_session == 0){
-                //get_current_week_display($system_date, $regular_sessions[RG])
+                get_current_week_display($system_date, $regular_sessions[RG]);
+            }
+            //
+            else if($is_date_in_session < 0){
+                $prev_term = get_prev_term($CFG->currentterm);
+                while(1){
+                    $query_result = registrar_query::run_registrar_query(
+                            'ucla_getterms', array($prev_term));
+                    $regular_sessions = find_regular_sessions($query_result);  
+                    
+                    if(isset($regular_sessions[RG])) {
+                        $is_date_in_session = 
+                                is_date_in_session($system_date,$regular_sessions[RG]);
+                        if($is_date_in_session == 0){
+                            get_current_week_display($system_date, $regular_sessions[RG]);
+                            break;
+                        } 
+                        if($is_date_in_session > 0){
+                            $query_result = registrar_query::run_registrar_query(
+                                    'ucla_getterms', array(get_next_term($prev_term)));
+                            $regular_sessions = find_regular_sessions($query_result);                              
+                            get_current_week_display($system_date, $regular_sessions[RG]);
+                            break;                            
+                        }
+                    }
+                    $prev_term = get_prev_term($prev_term);
+                }                
+            }
+            //If the date is in a future session
+            else{ // $is_date_in_session > 0
+                
+                $next_term = get_next_term($CFG->currentterm);
+                while(1){
+                    $query_result = registrar_query::run_registrar_query(
+                            'ucla_getterms', array($next_term));
+                    $regular_sessions = find_regular_sessions($query_result);  
+                    
+                    if(isset($regular_sessions[RG])) {
+                        $is_date_in_session = 
+                                is_date_in_session($system_date,$regular_sessions[RG]);
+                        if($is_date_in_session >= 0){
+                            get_current_week_display($system_date, $regular_sessions[RG]);
+                            break;
+                        } 
+                    }
+                    $next_term = get_next_term($next_term);
+                }
             }
         }
-        //else if();
+
         
     }
 
+   /**
+    * Takes in a UCLA term (Ex: 11F) and returns the term after it.
+    */       
+    function get_next_term($current_term){
+        $year = intval(substr($current_term,0 , 2));
+        $quarter = $current_term[3];
+        switch($quarter){
+            case 'F':
+                return $year."W";
+            case 'W':
+                $next_year = ($year == 99) ? "00" : sprintf("%02d", intval($year)+1);
+                return $next_year."S";
+            case 'S':
+                return $year."1";
+            case '1':
+                return $year."F";
+        }
+    }
+
+   /**
+    * Takes in a UCLA term (Ex: 11F) and returns the term before it.
+    */       
+    function get_prev_term($current_term){
+        $year = intval(substr($current_term,0 , 2));
+        $quarter = $current_term[3];
+        switch($quarter){
+            case 'F':
+                return $year."1";
+            case 'W':                
+                return $year."F";
+            case 'S':
+                $prev_year = ($year == 0) ? "99" : sprintf("%02d", intval($year)-1);
+                return $prev_year."W";
+            case '1':
+                return $year."S";
+        }
+    }    
    /**
     * Takes in a session and a date belonging to that session, and returns the
     * current_week_display string associated with the date
@@ -90,49 +168,84 @@ class block_ucla_weeksdisplay extends block_base {
     * for the various summer sessions.
     */      
     function get_current_week_display($date, $session){
-        //Array whose index represents the month (1 = January, etc) and value
-        //has the number of days in that month. Februrary is defined as 28 days.
-        $days_per_month[1] = 31;
-        // <editor-fold defaultstate="collapsed" desc="Rest of days_per_month declaration">        
-        $days_per_month[2] = 28;
-        $days_per_month[3] = 31;
-        $days_per_month[4] = 30;
-        $days_per_month[5] = 31;
-        $days_per_month[6] = 30;
-        $days_per_month[7] = 31;
-        $days_per_month[8] = 31;
-        $days_per_month[9] = 31;
-        $days_per_month[10] = 31;
-        $days_per_month[11] = 30;
-        $days_per_month[12] = 31; // </editor-fold>
-
-        $session_start_date = $session[5];
-        $session_end_date = $session[6];
-        $instruction_start_date = $session[7];
- 
-        $date_vs_session_start = cmpdate($date, $session_start_date);
-        $date_vs_end = cmpdate($date, $session_end_date);       
-        $date_vs_instruction_start = cmpdate($date, $instruction_start_date);
+        $ses_start_date = $session[5];
+        $ses_end_date = $session[6];
+        $instr_start_date = $session[7];
         
-  
-        $parsed_date = parse_date($date);
-        $parsed_session_start_date = parse_date($session[5]);
-        $parsed_session_end_date = parse_date($session[6]);
-        $parsed_instruction_start_date = parse_date($session[7]);        
+        $unix_date = strtotime($date);
+        $unix_ses_start_date = strtotime($session[5]);
+        $unix_ses_end_date = strtotime($session[6]);
+        $unix_instr_start_date = strtotime($session[7]); 
+        
+        $date_vs_ses_start = find_earlier_date($date, $session_start_date);
+        $date_vs_ses_end = find_earlier_date($date, $session_end_date);       
+        $date_vs_instr_start 
+            = find_earlier_date($date, $instruction_start_date);
+        $ses_start_vs_instr_start  
+            = find_earlier_date($session_start_date, $instruction_start_date);
+         
         //If the date is in Week 0.
-        if($date_vs_session_start >= 0 && $date_vs_instruction_start < 0){
+        if($date_vs_ses_start >= 0 && $date_vs_instr_start < 0){
             return ucla_term_to_text($session[0])." - Week 0";
         }
-        else if($date_vs_instruction_start >= 0 && $date_vs_session_end >= 0){
-            //$parsed_instruction_start_date
+        // If the date is in Week 1- Finals Week
+        else if($date_vs_instr_start >= 0 && $date_vs_ses_end <= 0){
+            //TODO: Summer sessions: ses start == instr start?
+            
+            //Week 1 always starts the first monday after session start date.
+            //TODO: overflow stuff
+            $first_day_of_first_week = date("z", $unix_ses_start_date);
+            // <editor-fold defaultstate="collapsed" desc="Find first day of first week">
+            switch (get_dayofweek($ses_start_date)) {
+                case "Mon":
+                    $first_day_of_first_week += 7;
+                    break;
+                case "Tue":
+                    $first_day_of_first_week += 6;
+                    break;
+                case "Wed":
+                    $first_day_of_first_week += 5;
+                    break;
+                case "Thu":
+                    $first_day_of_first_week += 4;
+                    break;
+                case "Fri":
+                    $first_day_of_first_week += 3;
+                    break;
+                case "Sat":
+                    $first_day_of_first_week += 2;
+                    break;
+                case "Sun":
+                    $first_day_of_first_week += 1;
+                    break;
+            }// </editor-fold>
+            
+            //Find the number of weeks elapsed from the first day to the current day.
+            $weeks_from_first_day 
+                = ((date("z", $date) - $first_day_of_first_week) % 7) + 1;
+            
+            return ucla_term_to_text($session[0])." - Week ". $weeks_from_first_day;            
             
         }
-        //else if()
+        else{
+            return ucla_term_to_text($session[0]);
+        }
         
+    }
+
+   /**
+    * Returns whether or not the date is within the dession.
+    * @param date string that starts with the format YYYY-MM-DD
+    * @return a string that represents the day of the week associated with date.
+    *  (Monday, Tuesday, etc.)
+    */     
+    function get_dayofweek($date){
+        $unix_date = strtotime($date);
+        return date($unix_date, 'D');
     }
     
    /**
-    * Compare
+    * Returns whether or not the date is within the dession.
     * @param date string that starts with the format YYYY-MM-DD
     * @param session a session object returned by ucla_getterms registrar query
     * @return 1 if date comes after the session's session_end date.
@@ -144,8 +257,8 @@ class block_ucla_weeksdisplay extends block_base {
         $session_start_date = $session[5];
         $session_end_date = $session[6];
         
-        $date_vs_start = cmpdate($date, $session_start_date);
-        $date_vs_end = cmpdate($date, $session_end_date);
+        $date_vs_start = find_earlier_date($date, $session_start_date);
+        $date_vs_end = find_earlier_date($date, $session_end_date);
         
         //If the date comes after start of session and before end of session
         //("after" and "before" include if date is the same as session dates)        
@@ -221,56 +334,98 @@ class block_ucla_weeksdisplay extends block_base {
         }
         
         return $regular_sessions;
-    }
+    }  
 
    /**
-    * @param date string that starts with the format YYYY-MM-DD
-    * @return a date object with indices year,month,day.
-    * Date['year'] = YYYY
-    * Date['month'] = MM
-    * Date['day'] = DD
-    */
-    function parse_date($date){
+    * Compares 2 dates and returns which one is earlier and by how many days.
+    * @param date1, date2  strings that start with the format YYYY-MM-DD
+    * @return a positive number denoting how many days date1 comes after date2.
+    *         0 if date1 is the same as date2
+    *         a negative number denoting how many days date1 comes before date2.
+    */   
+    function cmp_dates($date1, $date2){
+        $unix_date1 = strtotime($date1);
+        $unix_date2 = strtotime($date2);  
         
-        $date_obj['year'] = intval(substr($date, 0, 4));
-        $date_obj['month'] = intval(substr($date, 5, 2));        
-        $date_obj['day'] = intval(substr($date, 8, 2));    
-        return $date_obj;
+        $days_difference = 0; //The amount of days between date1 and date2. 
+                            //Positive = date1 comes after date2
+                            //Negative = date1 comes before date1.
+        
+        $earlier_date_result = find_earlier_date($date1, $date2);
+        if($earlier_date_result == 0){
+            return 0;
+        } else if($earlier_date_result == -1){
+            $earlier_date = $unix_date1;
+            $later_date = $unix_date2;
+        } else { // $earlier_date_result == 1
+            $earlier_date = $unix_date2;
+            $later_date = $unix_date1;            
+        }
+        
+        $earlier_date_year = date('Y',$unix_date1);
+        $later_date_year = date('Y',$unix_date2);        
+        //Traverse years until the two years are equal.
+        if($earlier_date_year < $later_date_year) { 
+            //Round the earlier date to beginning of the next year to make calculations easier.
+            $unix_date1_day_of_year = date('z',$unix_date1);
+            $days_in_year = (date('L',$unix_date1)) ? 366 : 365;
+            //+1 because the unix date day of year starts at 0.
+            $days_difference += ($days_in_year - $unix_date1_day_of_year + 1);
+            $unix_date1_year++;
+            
+            //Traverse whole years until you reach date2's year.
+            while($unix_date1_year < $unix_date2_year){
+                $days_in_year = (is_leap_year($unix_date1_year)) ? 366 : 365;
+                $days_difference += ($days_in_year + 1);
+                $unix_date1_year++;
+            }
+        } 
+        //Add the number of days from the beginning of the year to the later date.
+        $unix_date2_day_of_year = date('z',$unix_date2);
+        $days_in_year = (is_leap_year($unix_date1_year)) ? 366 : 365;
+        $days_difference += ($days_in_year - $unix_date2_day_of_year + 1);
+        
+        //Return a negative number if date1 comes before date2.
+        if($earlier_date_result == -1){ $days_difference *= -1;}
+        
+        return $days_difference;
     }    
 
+    function is_leap_year($year){
+        return ($year % 4) ? 1 : 0;
+    }
+    
    /**
     * Compares 2 dates and returns which one is earlier.
     * @param date1, date2  strings that start with the format YYYY-MM-DD
     * @return 1 if date1 comes after date2.
     *         0 if date1 is the same as date2
     *         -1 if date1 comes before date2.
-    */   
-    function cmp_dates($date1, $date2){
-        $date1_obj = parse_date($date1);
-        $date2_obj = parse_date($date2);
+    */      
+    function find_earlier_date($date1, $date2){   
+        $unix_date1 = strtotime($date1);
+        $unix_date2 = strtotime($date2); 
+        $unix_date1_year = date("Y", $unix_date1);
+        $unix_date2_year = date("Y", $unix_date2);
         
-        if($date1_obj['year'] > $date2_obj['year']) { 
+        
+        if($unix_date1_year > $unix_date2_year) { 
             return 1; 
-        } else if($date1_obj['year'] < $date2_obj['year']) { 
+        } else if($unix_date1_year < $unix_date2_year) { 
             return -1; 
         } else { //$date1 year == $date2 year            
-            
-            if($date1_obj['month'] > $date2_obj['month']){
+            $unix_date1_day_in_year = date("z", $unix_date1);
+            $unix_date2_day_in_year = date("z", $unix_date2);            
+            if($unix_date1_day_in_year > $unix_date2_day_in_year){
                 return 1;
-            } else if($date1_obj['month'] < $date2_obj['month']){
+            } else if($unix_date1_day_in_year < $unix_date2_day_in_year){
                 return -1;
-            } else { //$date1_month == $date2_month
-                               
-                if($date1_obj['day'] > $date2_obj['day']){
-                    return 1;
-                } else if($date1_obj['day'] < $date2_obj['day']){
-                    return -1;
-                } else{
-                    return 0;
-                }
-            }     
-        }   
-    }    
+            } else { //$unix_date1_day_in_year == $unix_date2_day_in_year
+                return 0;
+            }
+        }     
+     }
+    
     
 }
 
--- testRegistrar.php
+++ testRegistrar.php
@@ -3,7 +3,7 @@
 $db_conn = odbc_connect('RIGEL-TEST', 'ccle_dev', 'KK3456Qwd') or die( "ERROR: C
 onnection to Registrar failed.");
 
-$result = odbc_exec ($db_conn, "EXECUTE ucla_getterms '10W'");
+$result = odbc_exec ($db_conn, "EXECUTE ucla_getterms '101'");
 while (odbc_fetch_into($result, $row))
 {
     print_r($row);
