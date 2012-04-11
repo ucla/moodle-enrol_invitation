62ab99da2236d34a7e7a6d123433acea6c8f0f5d
Yangmun Choi <yangmungi@ucla.edu>
2012-04-11 16:35:25 -0700

    CCLE-2893 - Fixing bad settings code that caused crash in course requestor.

--- admin/tool/uclacourserequestor/settings.php
+++ admin/tool/uclacourserequestor/settings.php
@@ -7,11 +7,3 @@ $ADMIN->add('courses', new admin_externalpage('uclacourserequestor',
     "$CFG->wwwroot/$CFG->admin/tool/uclacourserequestor/index.php",
     'tool/uclacourserequestor:edit'));
 
-if ($ADMIN->fulltree) {
-    $settings->add(new admin_setting_configtext(
-        'tool_uclacourserequest/customfilters',
-        get_string('customfilters', 'tool_uclacourserequestor'),
-        get_string('customfilters_desc', 'tool_uclacourserequestor'),
-        ''
-    ));
-}
