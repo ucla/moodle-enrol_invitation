31df7ec8f13562c98964bb32c46a1ddcf40c2eb8
Matthew McKeen <mmckeen@oid.ucla.edu>
2012-04-23 15:40:18 -0700

    CCLE-2314 - BRUINCAST BLOCK - Further work on flushing out the block, working for cases where srs is correct

--- blocks/ucla_bruincast/block_ucla_bruincast.php
+++ blocks/ucla_bruincast/block_ucla_bruincast.php
@@ -2,54 +2,15 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-class block_ucla_bruincast extends block_base {
+require_once(dirname(dirname(dirname(__FILE__))) . '/local/ucla/lib.php');
 
-    private $active; 
+class block_ucla_bruincast extends block_base {
 
     /**
       * Called by moodle
       **/
     public function init() {
-        global $DB, $COURSE; // get global variables
-      
-        $this->active = false; // fallback on $active
-
-        /** check if block should be set active **/
-        $courseid =$COURSE->id;
-        $coursefound = $DB->get_record('course', array('id'=>$courseid)); // get course record in db
         
-        if (!empty($coursefound)) {
-            $coursesrs = substr($coursefound->idnumber, strpos($coursefound->idnumber,'-')+1); // generate the courseid as in bruincast table
-           
-            if (!empty($coursesrs)) {
-                $bcnodes = $DB->get_record('ucla_bruincast', array('srs'=>$coursesrs)); // get entry in bruincast table by the generated courseid
-            }
-
-            if (!empty($bcnodes)) {
-
-                // get course record again, uses info from bc table for double checking      
-                $course = $DB->get_record('course', array('idnumber'=>($bcnodes->term.'-'.$bcnodes->srs))); 
-
-                if (!empty($course) && !empty($bcnodes->bruincast_url)) {
-
-                    if (strcmp($bcnodes->restricted, "Restricted")==0) {
-
-                        // get contexts for permission checking
-                        $context = get_context_instance(CONTEXT_COURSE, $courseid); 
-                    
-                        // check if has permission, set block to active if does
-                        if (is_enrolled($context) || has_capability('moodle/site:config', $context)) {
-                            $this->active = true;
-                        }
-    
-                    } else { // if not restricted, no need for restriction checking
-                        $this->active = true;
-                    }                    
-     
-                }
-            }   
-        }
-
         // initialize title and name
         $this->title = get_string('title', 'block_ucla_bruincast');
         $this->name = get_string('pluginname', 'block_ucla_bruincast');
@@ -61,29 +22,12 @@ class block_ucla_bruincast extends block_base {
     public function get_content() {
         global $COURSE;
 
-        $courseid = $COURSE->id;
-        $context = get_context_instance(CONTEXT_BLOCK, $courseid);
-
         if ($this->content !== null) {
             return $this->content;
         }
             
         $this->content = new stdClass;
         
-        // hide block if not is role block/bruincast:viewblock
-        if (!has_capability('block/bruincast:viewblock', $context)) {
-            return $this->content;
-        }
-
-        // set block content by whether or not course contents found
-        if ($this->active) {
-            $this->content->text   = get_string('contfound', 'block_ucla_bruincast');
-        } else {
-            $this->content->text   = get_string('contnotfound', 'block_ucla_bruincast');
-        }
-
-        $this->content->footer     = get_string('footer', 'block_ucla_bruincast');
-
         return $this->content;
     }
 
@@ -95,46 +39,44 @@ class block_ucla_bruincast extends block_base {
         global $DB;
                                     
         $courseid = $course->id; // course id from the hook function
-        $coursefound = $DB->get_record('course', array('id'=>$courseid)); // get course record in db
+        $coursefound = ucla_map_courseid_to_termsrses($courseid);
 
         $nodes = array(); // initialize $nodes with an empty array for a good fallback; the empty array has no effect in coursemenu block hook.
 
-        $present = false;
+        if(!empty($coursefound)) {
 
-        // name has to be hardcoded, no way since $this references coursemenu on hook
-        if ($this->page->blocks->is_block_present("ucla_bruincast")) {
-            $present = true;
-        }
+        foreach($coursefound as $courseentry) {
 
-        if (!empty($coursefound) && $present) {
-            $coursesrs = substr($coursefound->idnumber, strpos($coursefound->idnumber,'-')+1); // generate the courseid as in bruincast table
+                $coursesrs = $courseentry->srs; // generate the courseid as in bruincast table
+                $courseterm = $courseentry->term;
            
-            if (!empty($coursesrs)) {
-                $bcnodes = $DB->get_record('ucla_bruincast', array('srs'=>$coursesrs)); // get entry in bruincast table by the generated courseid
-            }
+                if (!empty($coursesrs)) {
+                    $bcnodes = $DB->get_record('ucla_bruincast', array('srs'=>$coursesrs, 'term'=>$courseterm)); // get entry in bruincast table by the generated courseid
+                }
 
-            if (!empty($bcnodes)) {
-                // get course record again, uses info from bc table for double checking      
-                $course = $DB->get_record('course', array('idnumber'=>($bcnodes->term.'-'.$bcnodes->srs))); 
+                if (!empty($bcnodes)) {
 
-                if (!empty($course) && !empty($bcnodes->bruincast_url)) {
+                    if (!empty($bcnodes->bruincast_url)) {
 
-                    if(strcmp($bcnodes->restricted, "Restricted") == 0){
-                        // get contexts for permission checking
-                        $context = get_context_instance(CONTEXT_COURSE, $courseid); 
-                        $usercontext = get_context_instance(CONTEXT_USER, $courseid);
+                        if(strcmp($bcnodes->restricted, "Restricted") == 0){
+                            // get contexts for permission checking
+                            $context = get_context_instance(CONTEXT_COURSE, $courseid); 
+                            $usercontext = get_context_instance(CONTEXT_USER, $courseid);
                     
-                        // check if has permission, then generate menu nodes if does
-                        if(is_enrolled($context) || has_capability('moodle/site:config', $context)){
-                            $nodes = array(navigation_node::create('Bruincast', new moodle_url($bcnodes->bruincast_url)));
-                        }
+                            // check if has permission, then generate menu nodes if does
+                            if(is_enrolled($context) || has_capability('moodle/site:config', $context)){
+                                $nodes[] = navigation_node::create('Bruincast', new moodle_url($bcnodes->bruincast_url));
+                            }
 
-                    } else { // if not restricted, no need for restriction checking, just generate nodes
-                        $nodes = array(navigation_node::create('Bruincast', new moodle_url($bcnodes->bruincast_url)));
-                    }
-                }                    
-            }   
+                        } else { // if not restricted, no need for restriction checking, just generate nodes
+                            $nodes[] = navigation_node::create('Bruincast', new moodle_url($bcnodes->bruincast_url));
+                        }
+                    }                    
+                }   
+        
         }        
+        
+        }
         return $nodes;
     } 
 
@@ -142,7 +84,15 @@ class block_ucla_bruincast extends block_base {
       *  Called by moodle
       **/
    public function applicable_formats() {
-        return array('course'=> true); // block only available on course pages
+   
+        return array(
+            'site-index' => false,
+            'course-view'=> false,
+            'my' => false,
+            'block-ucla_bruincast' => false,
+            'not-really-applicable' => true
+        );
+        // hack to make sure the block can never be instantiated
    }
 
     /** 
