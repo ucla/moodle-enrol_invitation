b8ac6150e7cd9adbd62bd907f54dbc123a9a9311
Yangmun Choi <yangmungi@ucla.edu>
2012-04-26 16:48:16 -0700

    CCLE-2283 - Enabling friendly urls for dev, test, stage and prod, for CCLE shared.

--- local/ucla/config/shared_dev_moodle-config.php
+++ local/ucla/config/shared_dev_moodle-config.php
@@ -87,6 +87,13 @@ $CFG->instructor_levels_roles = array(
 // Course builder
 $terms_to_built = array('12S', '121', '12F');
 
+// CCLE-2283: Friendly URLs
+$CFG->ucla_friendlyurls_enable = true;
+
+// CCLE-2283: Redirect to archive (these have a high chance of changing)
+$CFG->remotetermcutoff = '12S';
+$CFG->archiveserver = 'https://archive.ccle.ucla.edu';
+
 // Course Requestor
 $CFG->forced_plugin_settings['tool_uclacourserequestor']['terms'] = $terms_to_built;
 $CFG->forced_plugin_settings['tool_uclacourserequestor']['selected_term'] = $CFG->currentterm;
