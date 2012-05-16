6d85af1a5b52148306959b3fec67eac9c53be397
Matthew McKeen <mmckeen@oid.ucla.edu>
2012-05-16 14:23:43 -0700

    CCLE-2314 - BRUINCAST BLOCK - Slight refactor of the settings

--- blocks/ucla_bruincast/settings.php
+++ blocks/ucla_bruincast/settings.php
@@ -4,24 +4,30 @@
  * Generates the settings form for the Bruincast Block
  */
 
-$settings->add(new admin_setting_configcheckbox(
-            'quiet_mode',
-            get_string('headerquietmode','block_ucla_bruincast'),
-            get_string('descquietmode','block_ucla_bruincast'),
-            '0'
-        ));
+defined('MOODLE_INTERNAL') || die;
 
-$settings->add(new admin_setting_configtext(
-            'bruincast_data',
-            get_string('headerbruincasturl','block_ucla_bruincast'),
-            get_string('descbruincasturl','block_ucla_bruincast'),
-            'http://www.oid.ucla.edu/help/info/bcastlinks/',
-            PARAM_TEXT 
-        ));
-$settings->add(new admin_setting_configtext(
-            'bruincast_errornotify_email',
-            get_string('headererrornotifyemail','block_ucla_bruincast'),
-            get_string('descerrornotifyemail','block_ucla_bruincast'),
-            '',
-            PARAM_TEXT
-        ));
+if($ADMIN->fulltree) {
+
+    $settings->add(new admin_setting_configcheckbox(
+                'quiet_mode',
+                get_string('headerquietmode','block_ucla_bruincast'),
+                get_string('descquietmode','block_ucla_bruincast'),
+                '0'
+            ));
+
+    $settings->add(new admin_setting_configtext(
+               'bruincast_data',
+                get_string('headerbruincasturl','block_ucla_bruincast'),
+                get_string('descbruincasturl','block_ucla_bruincast'),
+                'http://www.oid.ucla.edu/help/info/bcastlinks/',
+                PARAM_TEXT 
+            ));
+    $settings->add(new admin_setting_configtext(
+                'bruincast_errornotify_email',
+                get_string('headererrornotifyemail','block_ucla_bruincast'),
+                get_string('descerrornotifyemail','block_ucla_bruincast'),
+                '',
+                PARAM_TEXT
+            ));
+
+}
