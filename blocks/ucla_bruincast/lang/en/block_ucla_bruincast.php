42a63608ef47b1bdb170785ee835eac89f06b9d7
Matthew McKeen <mmckeen@oid.ucla.edu>
2012-05-16 13:59:34 -0700

    CCLE-2314 - BRUINCAST BLOCK - Cleanup of uneeded files

--- blocks/ucla_bruincast/lang/en/block_ucla_bruincast.php
+++ blocks/ucla_bruincast/lang/en/block_ucla_bruincast.php
@@ -2,11 +2,6 @@
     $string['title'] = 'UCLA bruincast';
     $string['pluginname'] = "UCLA bruincast";
 
-    $string['contfound'] = 'Bruincast integration enabled.  Link is displayed in Course menu given proper user permissions.';
-    $string['contnotfound'] = 'Bruincast integration enabled, but course content is not found.  Link is not displayed in Course menu.';
-
-    $string['footer'] = "This block is hidden for non-admin users.";
-
     $string['headerbruincasturl'] = "Source URL";
     $string['descbruincasturl'] = "CSV source URL from the OID website";
 
