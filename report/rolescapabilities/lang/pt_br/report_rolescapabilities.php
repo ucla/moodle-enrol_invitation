f3799389f5e85aa49866398ba236aa17f7f1023b
Rex Lorenzo <rex@oid.ucla.edu>
2012-05-21 12:19:05 -0700

    CCLE-3061 - Install admin report to compare roles

---
+++ report/rolescapabilities/lang/pt_br/report_rolescapabilities.php
@@ -0,0 +1,13 @@
+<?php
+
+$string['config_available_roles'] = 'Funções que são mostradas no relatório Permissões dos Papéis';
+$string['desc_available_roles'] = 'Escolha quais papéis serão disponibilizados para o usuário que pode ver o relatório.';
+$string['legend_title'] = 'Legenda de cores.';
+$string['no_roles_selected'] = 'Nenhum papel foi selecionado. Por favor, selecione ao menos um papel para mostrar as permissões.';
+$string['repeat_each'] = 'Repetir o cabeçalho a cada N registros';
+$string['rolescapabilities'] = 'Permissões dos papéis';
+$string['rolescapabilities:view'] = 'Visualizar relatório administrativo de Permissões dos papéis';
+$string['pluginname'] = 'Permissões dos papéis';
+$string['no_roles_available'] = 'Nenhum papel disponível';
+
+?>
---
+++ report/rolescapabilities/version.php
@@ -0,0 +1,8 @@
+<?php
+
+$plugin->version  = 2011100600;
+$plugin->release  = 0.1;
+$plugin->requires = 2010112400;
+$plugin->maturity = MATURITY_STABLE;
+
+?>
