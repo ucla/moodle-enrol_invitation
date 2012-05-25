ucla/moodle-enrol_invitation

commit 6996ab40603e526d3fe2b2c94c7ae9005ad3f3f3

CCLE-2312 - LIBRARY RESERVES BLOCK - fixed displaying of library reserve link so that if multiple urls are found that the first one still has the subject area and catalog number appended to it

blocks/ucla_library_reserves/block_ucla_library_reserves.php
@@ -1,5 +1,4 @@
 <?php
-
 defined('MOODLE_INTERNAL') || die();
 
 require_once(dirname(dirname(dirname(__FILE__))) . '/local/ucla/lib.php');
@@ -7,21 +6,18 @@ require_once(dirname(dirname(dirname(__FILE__))) . '/local/ucla/lib.php');
 class block_ucla_library_reserves extends block_base {
 
     /**
-      * Called by moodle
-      **/
+     * Called by moodle
+     */
     public function init() {
-
         // Initialize name and title
-        $this->title = get_string('title','block_ucla_library_reserves');
-
+        $this->title = get_string('title', 'block_ucla_library_reserves');
     }
 
     /**
-      * Called by moodle
-      **/
+     * Called by moodle
+     */
     public function get_content() {
         global $COURSE;
-
 
         if ($this->content !== null) {
             return $this->content;
@@ -33,118 +29,93 @@ class block_ucla_library_reserves extends block_base {
     }
 
     /**
-      * Use UCLA Course menu block hook
-      **/
+     * Use UCLA Course menu block hook
+     */
     public function get_navigation_nodes($course) {
-
         // get global variables
-        global $DB,$COURSE;
+        global $DB, $COURSE;
+
+        $nodes = array();
+        $links = array();        
         
         $courseid = $COURSE->id; // course id from the hook function
-        $coursefound = ucla_map_courseid_to_termsrses($courseid); 
-       
-        $nodes = array();
-        $lrnodes = array();
-        $previouslinks = array();
-        $atleastone = false; 
+        $courseinfo = ucla_get_course_info($courseid);
+        
+        if (empty($courseinfo)) {
+            return $nodes;
+        }
+                
+        foreach ($courseinfo as $index => $courseentry) {
+            // see if term/srs is found in library_reserves table
+            $lrnodes = $DB->get_records('ucla_library_reserves',
+                    array('quarter' => $courseentry->term, 'srs' => $courseentry->srs)); 
 
-        if (!empty($coursefound)) {
-
-            foreach($coursefound as $courseentry) {
-           
-                $coursesrs = $courseentry->srs; // generate the courseid as in libraryreserves table
-                $courseterm = $courseentry->term;
-
-                if (!empty($coursesrs) && !empty($courseterm)) {
-                    $lrnodes = $DB->get_records('ucla_library_reserves', array('quarter'=>$courseterm,'srs'=>$coursesrs)); // get entry in libraryreserves table by the generated courseid
-
-                    if($lrnodes) {
-                        $foundviasrs = true;
-                    }
-
-                    $foundviasrs = false;
-
-                    $reginfo = ucla_get_reg_classinfo($courseterm, $coursesrs);
-
-                    // If srs did not work as lookup, use the term, courseid, and department code 
-                    if($lrnodes == false) {
-
-                        if(!empty($reginfo)) {
-                            $lrnodes = $DB->get_records('ucla_library_reserves', array('quarter'=>$courseterm, 'department_code'=>$reginfo->subj_area, 'course_number'=>$reginfo->coursenum));
-                        }
-                    }
-                }
-
-
-                if($lrnodes != false) {
-
-                    foreach($lrnodes as $lrnode) {
-
-                        $reginfo = ucla_get_reg_classinfo($lrnode->quarter,$lrnode->srs);
-
-                        $sectnum = $reginfo->sectnum;
-                        $coursesectnum = substr($COURSE->shortname,strrpos($COURSE->shortname, "-") + 1);
-
-                        if (!empty($lrnode->url) && ($sectnum == $coursesectnum || $foundviasrs)) {
-                            $usedlink = false;
-
-                            //check if already a link
-                            foreach($previouslinks as $link){
-                                if($link == $lrnode->url){
-                                    $usedlink = true;
-                                    break;
-                                }
-                            }
-                            
-                            if($usedlink || sizeof($previouslinks) == 0){
-                                if(!$atleastone)
-                                {
-                                    $nodes[] = navigation_node::create('Library Reserves', new moodle_url($lrnode->url));
-                                }
-
-                                $atleastone = true;
-                            } else {
-                                // Must hardcode the naming string since this is a static function
-                                $nodes[] = navigation_node::create('Library Reserves '.$reginfo->subj_area.$reginfo->coursenum, new moodle_url($lrnode->url));
-                            }
-                                $previouslinks[] = $lrnode->url; 
-                        }
-                    }
-                }
+            // If srs did not work as lookup, use the term, courseid, and department code 
+            if (empty($lrnodes)) {
+                $lrnodes = $DB->get_records('ucla_library_reserves',
+                        array('quarter' => $courseentry->term, 
+                              'department_code' => $courseentry->subj_area, 
+                              'course_number' => $courseentry->coursenum));
+            }
+            
+            if (empty($lrnodes)) {                
+                continue;  // no record found for courseentry
+            }                
+            
+            /* since we found some library reserve entries, create link array
+             * in the format [reserve link] => ['subj_area'] 
+             *                                 ['coursenum']
+             */  
+            foreach ($lrnodes as $lrnode) {
+                $links[$lrnode->url]['subj_area'] = $courseentry->subj_area;
+                $links[$lrnode->url]['coursenum'] = $courseentry->coursenum;
             }
         }
 
+        // if only one unique url was found, then just give the name "Library reserves"
+        $lr_string = get_string('title', 'block_ucla_library_reserves');        
+        if (count($links) == 1) {
+            $nodes[] = navigation_node::create($lr_string,
+                            new moodle_url(array_pop(array_keys($links))));            
+        } else {
+            // else display link with subj_area and coursenum appended
+            foreach ($links as $url => $entry) {
+                $nodes[] = navigation_node::create(sprintf('%s %s %s', 
+                        $lr_string, $entry['subj_area'], $entry['coursenum']), 
+                        new moodle_url($url));                 
+            }            
+        }
+        
         return $nodes;
-
     }
 
     /**
-      * Called by moodle
-      **/
+     * Called by moodle
+     */
     public function applicable_formats() {
-        
+
         return array(
             'site-index' => false,
-            'course-view'=> false,
+            'course-view' => false,
             'my' => false,
             'block-ucla_library_reserves' => false,
             'not-really-applicable' => true
-        ); 
+        );
         // hack to make sure the block can never be instantiated
-        
     }
 
     /**
-      * Called by moodle
-      **/
+     * Called by moodle
+     */
     public function instance_allow_multiple() {
         return false; //disables multiple blocks per page
     }
 
     /**
-      * Called by moodle
-      **/
+     * Called by moodle
+     */
     public function instance_allow_config() {
         return false; // disables instance configuration
     }
+
 }

commit bcdfa7fa7b20e5215aa5f0ee852d3fc03d8a771e

CCLE-2938 - English 4W showing wrong library reserves link - fixed regression in commit 6996ab40603e526d3fe2b2c94c7ae9005ad3f3f3 that caused this bug to appear again

blocks/ucla_library_reserves/block_ucla_library_reserves.php
@@ -50,12 +50,28 @@ class block_ucla_library_reserves extends block_base {
             $lrnodes = $DB->get_records('ucla_library_reserves',
                     array('quarter' => $courseentry->term, 'srs' => $courseentry->srs)); 
 
-            // If srs did not work as lookup, use the term, courseid, and department code 
+            // If srs did not work as lookup, use the term, courseid, and 
+            // department code, but make sure that the term/srs for that record
+            // is the same section for the course entry (see CCLE-2938)
             if (empty($lrnodes)) {
-                $lrnodes = $DB->get_records('ucla_library_reserves',
+                $sql = "SELECT  *
+                        FROM    {ucla_library_reserves} AS lr, 
+                                {ucla_reg_classinfo} AS lr_rci,
+                                {ucla_reg_classinfo} AS ce_rci
+                        WHERE   lr.quarter=:quarter AND
+                                lr.department_code=:department_code AND
+                                lr.course_number=:course_number AND
+                                lr.quarter=lr_rci.term AND
+                                lr.srs=lr_rci.srs AND
+                                ce_rci.term=:courseentry_term AND
+                                ce_rci.srs=:courseentry_srs AND
+                                lr_rci.sectnum=ce_rci.sectnum";                               
+                $lrnodes = $DB->get_records_sql($sql,
                         array('quarter' => $courseentry->term, 
                               'department_code' => $courseentry->subj_area, 
-                              'course_number' => $courseentry->coursenum));
+                              'course_number' => $courseentry->coursenum,
+                              'courseentry_term' => $courseentry->term,
+                              'courseentry_srs' => $courseentry->srs));     
             }
             
             if (empty($lrnodes)) {                