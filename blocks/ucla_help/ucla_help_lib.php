f0dcb8f82a4a6999716ca10b09cc677c199a434b
Rex Lorenzo <rex@oid.ucla.edu>
2012-06-18 12:29:32 -0700

    Revert "Adding shib attributes to help ticket debugging info: HTTP_SHIB_GIVENNAME, HTTP_SHIB_PERSON_SURNAME, HTTP_SHIB_MAIL"
    Shib attributes are only returned for pages protected by shib (i.e. /auth/shibboleth).
    
    This reverts commit 764ec6428b3e13e22dc564a2b9b0b2461010272d.

--- blocks/ucla_help/ucla_help_lib.php
+++ blocks/ucla_help/ucla_help_lib.php
@@ -335,10 +335,7 @@ function create_help_message(&$fromform)
     USER_autosubscribe       = " . @$description['autosubscribe'][$USER->autosubscribe] . "
     USER_trackforums         = " . @$description['trackforums'][$USER->trackforums] . "
     USER_timemodified        = " . @date('r' , $USER->timemodified) . "
-    USER_screenreader        = " . @$description['screenreader'][$USER->screenreader] . "
-    HTTP_SHIB_GIVENNAME      = " . @$_SERVER['HTTP_SHIB_GIVENNAME'] . "
-    HTTP_SHIB_PERSON_SURNAME = " . @$_SERVER['HTTP_SHIB_PERSON_SURNAME'] . "
-    HTTP_SHIB_MAIL           = " . @$_SERVER['HTTP_SHIB_MAIL'];
+    USER_screenreader        = " . @$description['screenreader'][$USER->screenreader];
     $body .= "\n";
     
     // get logging records
