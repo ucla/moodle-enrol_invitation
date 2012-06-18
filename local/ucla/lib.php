abafd6f82c966dcb42e84d8e85c7907e5f4e7ea6
Yangmun Choi <yangmungi@ucla.edu>
2012-06-17 17:36:04 -0700

    CCLE-3154 - Making get_active_terms return a set of terms indexed by term instead of indexed by int.

--- local/ucla/lib.php
+++ local/ucla/lib.php
@@ -1122,7 +1122,7 @@ function get_active_terms() {
         foreach ($terms as $term) {
             $term = trim($term);
             if (ucla_validator('term', $term)) {
-                $ret_val[] = $term;
+                $ret_val[$term] = $term;
             }                              
         }
     }    
