64dc81c01f43ad169bf65768aaced490fe15c820
Rex Lorenzo <rex@oid.ucla.edu>
2012-07-05 12:51:49 -0700

    CCLE-2389 - Site Indicator - enabling $CFG->enablecourserequests

--- local/ucla/config/shared_dev_moodle-config.php
+++ local/ucla/config/shared_dev_moodle-config.php
@@ -220,6 +220,9 @@ $CFG->forced_plugin_settings['moodlecourse']['maxbytes'] = 1572864000;  // 1.5GB
 // CCLE-2903 - Don't set completion tracking to be course default
 $CFG->forced_plugin_settings['moodlecourse']['enablecompletion'] = 0;
 
+// Site administration > Courses > Course request
+$CFG->enablecourserequests = 1;
+
 // Site administration > Language > Language settings
 $CFG->langstringcache = false;
 
