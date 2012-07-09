7c7e7bcbfeb4c68e45f82f37f34790bbbbd1c762
Rex Lorenzo <rex@oid.ucla.edu>
2012-07-09 14:47:48 -0700

    CCLE-2389 - Site Indicator
    Per discussion with Deborah/Jonathan (7/9/2012), do not want to manually set all non-srs sites to be a certain default type.

--- admin/tool/uclasiteindicator/db/install.php
+++ admin/tool/uclasiteindicator/db/install.php
@@ -4,8 +4,10 @@ require_once($CFG->dirroot . '/admin/tool/uclasiteindicator/lib.php');
 
 function xmldb_tool_uclasiteindicator_install() {
     global $CFG, $DB;
-    
-    siteindicator_manager::find_and_set_collab_sites();
+  
+// per discussion with Deborah/Jonathan (7/9/2012), do not want to manually set
+// all non-srs sites to be a certain default type    
+//    siteindicator_manager::find_and_set_collab_sites();
 
     return true;
 }
