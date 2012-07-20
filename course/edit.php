15c2d65b975fde7cc48fc2697522358cb679f1d2
Rex Lorenzo <rex@oid.ucla.edu>
2012-07-20 13:55:30 -0700

    CCLE-3402/CCLE-3401 -  fixed bug in which sites that didn't have a collab site, but also didn't have srs numbers also didn't get their site type saved

--- course/edit.php
+++ course/edit.php
@@ -143,7 +143,9 @@ if ($editform->is_cancelled()) {
         // Save any changes to the files used in the editor
         update_course($data, $editoroptions);        
         // START UCLA MOD CCLE-2389 - update site indicator
-        if (is_collab_site($course)) {
+        // CCLE-3402/CCLE-3401 - only update if non-srs course
+        $is_course_site = ucla_map_courseid_to_termsrses($course->id);
+        if (empty($is_course_site)) {
             siteindicator_manager::update_site($data);
         }
         // END UCLA MOD CCLE-2389                
