ded4c091eaba4be6f5e5c9f4e6436b95f127870f
Alfonso Roman <aroman@oid.ucla.edu>
2012-07-31 09:27:25 -0700

    CCLE-2310 - moved header & body modules to own files
    need to define a common message scheme and tables

---
+++ blocks/ucla_alert/modules/alert_body_default.class.php
@@ -0,0 +1,57 @@
+<?php
+
+class ucla_alertblock_body_default extends ucla_alertblock_module {
+
+    function __construct($obj = null) {
+        $obj = new stdClass();
+        
+        $obj->mod = 'body';
+        $obj->type = 'default';
+        $obj->title = get_string('mod_body_default_title', 'block_ucla_alert');
+        $obj->content = array(
+            'main' => array(
+                array(
+                    'content' => 'this is a message',
+                    'start' => null,
+                    'end' => null,
+                ),
+                
+            ),
+            'list' => array(
+                array(
+                    'content' => 'Maecenas accumsan ante quis lacus pulvinar a rutrum nibh lobortis.',
+                    'color' => 'blue',
+
+                ),
+                array(
+                    'content' => 'Donec eu tortor vel sapien interdum viverra.',
+                    'link' => 'http://www.foo.com',
+                ),
+            ),
+        );
+        
+        $obj->start = null;
+        $obj->end = null;
+        
+        $this->defaults = array(
+            'list' => array(
+                'color' => 'default',
+            )
+        );
+        
+        parent::__construct($obj);
+    }
+    
+    function html_content() {
+        return $this->get_title() . $this->get_body();
+    }
+    
+    protected function get_title() {
+        
+        $title = html_writer::tag('div', $this->prop->title, 
+                array('class' => 'alert-block-body-title'));
+        
+        return $title;
+    }
+        
+}
