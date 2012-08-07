ddd675375ad4c1e6277ff81cfb082f24a9874cd6
terrencexy <terrencexy@hotmail.com>
2012-08-07 11:48:31 -0700

    CCLE-2867 - Vugs fixed in view.php, user can view the content without being logged in.

--- blocks/ucla_subject_links/view.php
+++ blocks/ucla_subject_links/view.php
@@ -12,12 +12,6 @@ require_once($CFG->dirroot.
     '/blocks/ucla_subject_links/block_ucla_subject_links.php');
 require_once($CFG->dirroot . '/local/ucla/lib.php');
 
-// Note that the unhiding of the Announcements forum is handled in
-// modules/email_students.php
-
-// Note that any logic unrelated to the display of the control panel should 
-// be handled within the module itself
-
 $courseid = required_param('course_id', PARAM_INT); // course ID
 $subjarea = required_param('subj_area', PARAM_TEXT);// subject area
 
@@ -25,36 +19,29 @@ if (! $course = $DB->get_record('course', array('id' => $courseid))) {
     print_error('coursemisconf');
 }
 
-require_login($course, true);
 $context = get_context_instance(CONTEXT_COURSE, $courseid);
 
 // Initialize $PAGE
 $PAGE->set_url('/blocks/ucla_subject_links/view.php', 
-    array('course_id' => $courseid));
+    array('course_id' => $courseid, 'subj_area' => $subjarea));
 
 $page_title = $course->shortname.': '.get_string('pluginname',
-    'block_ucla_control_panel');
+    'block_ucla_subject_links');
 
 $PAGE->set_context($context);
 $PAGE->set_title($page_title);
-
+$PAGE->set_course($course);
 $PAGE->set_heading($course->fullname);
 
 $PAGE->set_pagelayout('course');
 $PAGE->set_pagetype('course-view-'.$course->format);
 
 $location = $CFG->dirroot . '/blocks/ucla_subject_links/content/';
-$subjname = block_ucla_subject_links::subject_exist($course, $location);
 
-echo $OUTPUT->header();        
-if ($subjname != NULL) {
-    foreach ($subjname as $sub) {
-        $sub = strtoupper($sub);
-        include($location . $sub . '/index.htm');           
-    }
-}
+echo $OUTPUT->header();
+ 
+include($location . $subjarea . '/index.htm');           
             
 echo $OUTPUT->footer();
 
 /** eof **/
-
