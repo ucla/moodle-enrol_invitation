4decd643fbd3d178ee0a02ab148bf9dbfb011f0b
Rex Lorenzo <rex@oid.ucla.edu>
2012-08-17 12:33:09 -0700

    CCLE-2867 - Subject Area specific links in Site Menu block
    * Added/fixed link checker script

--- blocks/ucla_subject_links/link_checker.php
+++ blocks/ucla_subject_links/link_checker.php
@@ -1,69 +1,117 @@
 <?php
-function get_links($page) {
-    if($fp = fopen($page, 'r')) {
-        $content = "";
-        while ($line = fread($fp, 1025)) {
-            $content .= $line;
+/**
+ * Get all html files in given directory and parse them for html 
+ */
+// Script should only be run via CLI
+define('CLI_SCRIPT', true);
+
+require_once(dirname(__FILE__) . '/../../config.php');
+require_once($CFG->dirroot.'/blocks/moodleblock.class.php');
+require_once($CFG->dirroot. '/blocks/ucla_subject_links/block_ucla_subject_links.php');
+
+// script variables
+$show_debugging_messages = false;
+$timeout = 10;   // 10 seconds
+
+$directory = new RecursiveDirectoryIterator(block_ucla_subject_links::get_location());
+$iterator = new RecursiveIteratorIterator($directory);
+$regex = new RegexIterator($iterator, '/^.+\.htm$/i', RecursiveRegexIterator::GET_MATCH);
+
+// before checking links we need to set a timeout for fopen, or else bad urls
+// can hang the script
+$oldtimeout = ini_set('default_socket_timeout', $timeout);
+
+// files will be an array of paths to the html file we want to parse
+foreach ($regex as $files) {
+    foreach ($files as $file) {
+        cmd_debug('Working on ' . $file);
+        $links = checkPage(file_get_contents($file));
+        
+        if (empty($links)) {
+            cmd_debug('No links found');
+            continue;   // no links!
+        }
+        
+        foreach ($links as $link) {
+            // found links, now see if they are alive
+            cmd_debug('pinging link ' . $link);
+            if (pingLink($link)) {
+                cmd_debug('Link works! ' . $link);
+            } else {
+                echo sprintf("DEAD link found (%s) in %s\n", $link, $file);
+            }
         }
     }
+}
+
+ini_set('default_socket_timeout', $oldtimeout);
+echo "DONE!\n";
+
+
+// Script functions
+
+/**
+ * Given page content, will parse it to find html links.
+ * 
+ * @link http://www.phptoys.com/tutorial/create-link-checker.html original source
+ * 
+ * @param string $content
+ * @return array            Returns an array of links
+ */
+function checkPage($content) {
     $links = array();
-    $len = strlen($content); 
-    if ($len > 10) {
-        $start = 0;
+    $textLen = strlen($content);
+
+    if ($textLen > 10) {
+        $startPos = 0;
         $valid = true;
+
         while ($valid) {
-            set_time_limit(0);
-            $spos = strpos($content, '<a', $start);
-            if ($spos < $start) {
+            $spos = strpos($content, '<a ', $startPos);
+            if ($spos < $startPos)
                 $valid = false;
-                continue;
-            }
             $spos = strpos($content, 'href', $spos);
-            $spos = strpos($content, '"', $spos);
+            $spos = strpos($content, '"', $spos) + 1;
             $epos = strpos($content, '"', $spos);
-            $star = $epos;
-            $link = substr($content, $spos, $epos-$spos);
-            if (strpos($link, 'http://') !== false) {
+            $startPos = $epos;
+            $link = substr($content, $spos, $epos - $spos);
+            if ((strpos($link, 'http://') !== false) ||
+                    strpos($link, 'https://') !== false) {
                 $links[] = $link;
             }
         }
     }
+
     return $links;
 }
 
-function check_link($link) {
-    $file = @fopen($link, 'r');
-    $status = false;
-    if($file) {
-        $status = true;
+/**
+ * Throw away function because Moodle debugging() function is very cluttered
+ * when reading it from the command line.
+ */
+function cmd_debug($message) {
+    global $show_debugging_messages;
+    if ($show_debugging_messages) {
+        echo $message . "\n";
     }
-    fclose($file);
-    
+}
+
+/**
+ * Checks to see if URL exists.
+ * 
+ * @link http://www.phptoys.com/tutorial/create-link-checker.html original source
+ * 
+ * @param string $domain    URL to check
+ * @return boolean
+ */
+function pingLink($domain) {
+    $file = @fopen($domain, "r");
+    $status = false;
+
+    if (!empty($file)) {
+        $status = true;
+        fclose($file);
+    }
     return $status;
 }
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "DTD/xhtml1-transitional.dtd">
-<html>
-<body> 
-<?php
-    require_once(dirname(__FILE__).'/../../config.php');
-    global $CFG;
-    require_once($CFG->libdir.'/blocklib.php');
-    require_once($CFG->dirroot.'/blocks/moodleblock.class.php');
-    require_once($CFG->dirroot . '/blocks/ucla_subject_links/block_ucla_subject_links.php');
-    echo 'hi';
-    $dirs = scandir(block_ucla_subject_links::get_location());
-    unset($dirs[0]);
-    unset($dirs[1]);
-    foreach ($dirs as $dir) {
-        echo $dir;
-        $links = get_links(block_ucla_subject_links::get_location() . $dir . '/index.htm');
-        foreach ($links as $link) {
-            if (check_link($link) == false) {
-                echo $dir . '/index.htm' . ': "' . $link . '"';
-            }
-        }
-    }
-?> 
-</body>
-</html>
+
