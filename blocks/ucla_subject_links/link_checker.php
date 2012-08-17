2a702d6a74dbfd73d878e8e5f64207c1d36041db
Rex Lorenzo <rex@oid.ucla.edu>
2012-08-17 15:59:51 -0700

    CCLE-2867 - Subject Area specific links in Site Menu block
    * Getting link text to display in broken link report
    * Now using cURL to fake browser user agent and it is much faster now, since we aren't actually downloading the page content (but some links, like to the History department, show as false positives...

--- blocks/ucla_subject_links/link_checker.php
+++ blocks/ucla_subject_links/link_checker.php
@@ -10,17 +10,15 @@ require_once($CFG->dirroot.'/blocks/moodleblock.class.php');
 require_once($CFG->dirroot. '/blocks/ucla_subject_links/block_ucla_subject_links.php');
 
 // script variables
-$show_debugging_messages = false;
-$timeout = 10;   // 10 seconds
+$show_debugging_messages = true;
 $broken_links = array();    // should be indexed by file and then link
 
-$directory = new RecursiveDirectoryIterator(block_ucla_subject_links::get_location());
+$path_to_check = block_ucla_subject_links::get_location();
+$path_to_check = '/vagrant/moodle/blocks/ucla_subject_links/content/HIST';
+$directory = new RecursiveDirectoryIterator($path_to_check);
+
 $iterator = new RecursiveIteratorIterator($directory);
 $regex = new RegexIterator($iterator, '/^.+\.htm$/i', RecursiveRegexIterator::GET_MATCH);
-
-// before checking links we need to set a timeout for fopen, or else bad urls
-// can hang the script
-$oldtimeout = ini_set('default_socket_timeout', $timeout);
 
 // files will be an array of paths to the html file we want to parse
 foreach ($regex as $files) {
@@ -34,14 +32,14 @@ foreach ($regex as $files) {
         }
         
         $found_broken_link = false;
-        foreach ($links as $link) {
+        foreach ($links as $link => $link_text) {
             // found links, now see if they are alive
             cmd_debug('pinging link ' . $link);
             if (pingLink($link)) {
                 cmd_debug('Link works! ' . $link);
             } else {
                 cmd_debug(sprintf("DEAD link found (%s) in %s\n", $link, $file));
-                $broken_links[$file][] = $link;
+                $broken_links[$file][$link] = $link_text;
                 $found_broken_link = true;
             }
         }
@@ -58,10 +56,8 @@ foreach ($regex as $files) {
     }
 }
 
-ini_set('default_socket_timeout', $oldtimeout);
-
 // now display results
-echo "Broken links found:\n";
+echo "\nBroken links found:\n";
 print_r($broken_links);
 
 echo "DONE!\n";
@@ -70,12 +66,16 @@ echo "DONE!\n";
 // Script functions
 
 /**
- * Given page content, will parse it to find html links.
+ * Given page content, will parse it to find html links. Dependant on links
+ * being in following format:
+ * 
+ *  <a href="<link>"
  * 
  * @link http://www.phptoys.com/tutorial/create-link-checker.html original source
  * 
  * @param string $content
- * @return array            Returns an array of links
+ * @return array            Returns an array of links, in following format:
+ *                          [link] => [link text]
  */
 function checkPage($content) {
     $links = array();
@@ -96,7 +96,13 @@ function checkPage($content) {
             $link = substr($content, $spos, $epos - $spos);
             if ((strpos($link, 'http://') !== false) ||
                     strpos($link, 'https://') !== false) {
-                $links[] = $link;
+                // found link, so try to get link text
+                $start_link_text = strpos($content, '>', $epos) + 1;
+                $end_link_text = strpos($content, '<', $start_link_text);
+                
+                $link_text = substr($content, $start_link_text, $end_link_text - $start_link_text);
+                
+                $links[$link] = $link_text;
             }
         }
     }
@@ -118,19 +124,39 @@ function cmd_debug($message) {
 /**
  * Checks to see if URL exists.
  * 
- * @link http://www.phptoys.com/tutorial/create-link-checker.html original source
+
+ * @link http://stackoverflow.com/questions/981954/how-can-one-check-to-see-if-a-remote-file-exists-using-php/982045#982045 original source
  * 
  * @param string $domain    URL to check
  * @return boolean
  */
 function pingLink($domain) {
-    $file = @fopen($domain, "r");
-    $status = false;
+    $ch = curl_init($domain);
 
-    if (!empty($file)) {
-        $status = true;
-        fclose($file);
+    // Note, need to fake browser user agent, because some websites, like 
+    // (Humanities!) are setup to prevent bots from accessing their websites.
+    // Got this user agent from http://davidwalsh.name/set-user-agent-php-curl-spoof
+    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+    curl_setopt($ch, CURLOPT_NOBODY, true); // prevents html from being returned
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);  // set timeout or certain links will hang
+    curl_exec($ch);
+    
+    // first make sure that curl executed sucessfully
+    $curlretcode = curl_errno($ch);
+    if ($curlretcode > 0) {
+        cmd_debug('cURL error code: ' . $curlretcode);
+        return false;
     }
-    return $status;
+    
+    $retcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);    
+
+    // $retcode > 400 -> not found, $retcode = 200, found.    
+    if ($retcode >= 400) {
+        cmd_debug('HTTP error code: ' . $retcode);
+        return false;
+    }
+    
+    return true;    
 }
 
