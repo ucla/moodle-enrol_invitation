58935177af44b384f3496b8ae9d0902920131edc
Rex Lorenzo <rex@oid.ucla.edu>
2012-08-31 16:19:42 -0700

    CCLE-3472 - Lesson module problem - lesson shows no attempts even though gradebook records grades
    * Fixed query to properly match on groupingid

--- mod/lesson/report.php
+++ mod/lesson/report.php
@@ -45,14 +45,16 @@ $ufields = user_picture::fields('u'); // These fields are enough
 $params = array("lessonid" => $lesson->id);
 // TODO: Improve this. Fetching all students always is crazy!
 if (!empty($cm->groupingid)) {
-    $params["groupid"] = $cm->groupingid;
+// UCLA MOD: CCLE-3472 - Lesson module problem - lesson shows no attempts even though gradebook records grades    
+    $params["groupingid"] = $cm->groupingid;
     $sql = "SELECT DISTINCT $ufields
                 FROM {lesson_attempts} a
                     INNER JOIN {user} u ON u.id = a.userid
                     INNER JOIN {groups_members} gm ON gm.userid = u.id
-                    INNER JOIN {groupings_groups} gg ON gg.groupid = :groupid
+                    INNER JOIN {groupings_groups} gg ON (gg.groupingid = :groupingid AND gm.groupid=gg.groupid)
                 WHERE a.lessonid = :lessonid
                 ORDER BY u.lastname";
+// END UCLA MOD: CCLE-3472
 } else {
     $sql = "SELECT DISTINCT $ufields
             FROM {user} u,
