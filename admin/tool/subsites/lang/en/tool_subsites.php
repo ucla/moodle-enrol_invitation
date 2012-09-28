883d704ed47e65c718f6a4cf5d7becee8772bf4e
Yangmun Choi <yangmungi@ucla.edu>
2012-09-28 16:23:37 -0700

    CCLE-2386 - Removing deprecated implementation subsites.

--- admin/tool/subsites/lang/en/tool_subsites.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$string['pluginname'] = 'Sub-site management';
-$string['subsites:canhavesubsite'] = 'Can own a sub-site';
--- admin/tool/subsites/version.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-defined('MOODLE_INTERNAL') || die();
-
-$plugin->version = 2012092100;
-$plugin->component = 'tool_subsites';
