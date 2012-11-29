0b01736119d5a6258fc349af3369415dcee2c61b
Alfonso Roman <aroman@oid.ucla.edu>
2012-11-29 15:07:02 -0800

    CCLE-3679 - fixing bugs and adding better documentation

--- local/ucla/scripts/ph_survey_alert.php
+++ local/ucla/scripts/ph_survey_alert.php
@@ -13,25 +13,62 @@
 
 define('CLI_SCRIPT', true);
 
-require_once(dirname(__FILE__).'/../../config.php');
+require_once(dirname(__FILE__).'/../../../config.php');
 require_once($CFG->dirroot . '/blocks/ucla_alert/locallib.php');
 
-$sql = "SELECT t1.courseid FROM mdl_ucla_request_classes t1, mdl_ucla_reg_classinfo t2 where t1.term = t2.term and t1.srs = t2.srs and t2.division = 'PH' and t2.term = '12F'";
+echo "Adding alert notice to Humanities Fall 2012\n";
+///
+// Select all the course IDs where block will be installed
+$sql = "SELECT c.id
+    
+        FROM {course} c
+        JOIN {ucla_request_classes} AS urc ON urc.courseid = c.id
+        JOIN {ucla_reg_classinfo} AS rci ON rci.srs = urc.srs AND rci.term = urc.term
+        
+        WHERE 
+            rci.term = :term
+        AND rci.division = :division
+        AND urc.hostcourse = 1";
 
-$courseidarray = $DB->get_records_sql($sql, array('division'=>'PH', 'term'=>$CFG->currentterm));
+// Get records
+$records = $DB->get_records_sql($sql, array('term' => '12F', 'division' => 'PH'));
 
-$text = "# As part of our transition to a competencies-based curriculum, we are implementing an online course assessment system called SPHweb. This will replace the scantron course evaluations.  For Fall quarter 2012, we are asking you to complete end of quarter evaluations for your courses using the online system by Dec. 9. The online end-of-course surveys should be activated on your course websites effective immediately.  Please log on to the system using the this 
->{http://portal.ph.ucla.edu/sphweb/
-} survey link, then click [MyHome] and use your bol online login to access the system.";
 
-// How is this data going to look like?
-$o = array(
-    'courses' => $courseidarray,
-    'entity' => ucla_alert::ENTITY_ITEM,
-    'starts' => 'Dec 01 2012 12:00',
-    'expires' => 'Dec 09 2012 20:10',
-    'text' => $text
-);
-events_trigger('ucla_alert_post', $o);
-echo 'trigered event: ';
+// Put the IDs in an array
+$courseidarray = array();
+
+foreach($records as $r) {
+    $courseidarray[] = $r->id;
+}
+
+// Message we want to display
+$text = "# survey alert
+As part of our transition to a competencies-based curriculum, we are implementing an online course assessment system called SPHweb. This will replace the scantron course evaluations.  
+
+For Fall quarter 2012, we are asking you to complete end of quarter evaluations for your courses using the online system by Dec. 9. The online end-of-course surveys should be activated on your course websites effective immediately.  Please log on to the system using the link below, then click [MyHome] and use your bol online login to access the system.
+>{http://portal.ph.ucla.edu/sphweb/} Survey link";
+
+// Only apply if we have any records to display
+if(!empty($courseidarray)) {
+    // Create event data
+    $data = array(
+        'courses' => $courseidarray,
+        'entity' => ucla_alert::ENTITY_ITEM,
+        'starts' => 'Dec 01 2012 12:00',
+        'expires' => 'Dec 09 2012 20:10',
+        'text' => $text
+    );
+
+    // Trigger event
+    events_trigger('ucla_alert_post', $data);
+
+    // Display count and a list of course ids
+    $n = count($courseidarray);
+    
+    echo "There were $n courses alerted.\n";
+    echo "IDs: [ ". implode(', ', $courseidarray) . " ]\n";
+}
+
+echo "...done\n";
+
 /// END
