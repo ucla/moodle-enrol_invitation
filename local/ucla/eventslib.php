33303a5589ad1a805a3a9e32653d527d4ae2f5a3
Rex Lorenzo <rex@oid.ucla.edu>
2013-01-10 17:52:49 -0800

    CCLE-3556 - Hiding section doesn't hide material in section
    * reverted commit d94889a0518e08a4123cb7d27fcf2fc39bd80155
    * removed events that were previous added as part of previous commit 
    * made easy upload suite properly set visibility of content to parent's visibility

--- local/ucla/eventslib.php
+++ local/ucla/eventslib.php
@@ -3,32 +3,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 /**
- * Responds to the mod_created/mod_updated to make sure the course module's 
- * visiblity is the same as its parent section.
- * 
- * Fixes bug CCLE-3556 - Hiding section doesn't hide material in section
- * 
- * @param object $mod
- */
-function check_mod_parent_visiblity($mod) {
-    global $DB;
-    // needs to be fast, so just do everything in one database query
-    $sql = "UPDATE  {course_modules} AS cm
-            INNER JOIN  {course_sections} as cs ON (cm.section=cs.id)
-            SET     cm.visible=
-                CASE WHEN(cs.visible = 0)
-                    THEN 0
-                WHEN (cs.visible = 1)
-                    THEN cm.visible
-                END
-            WHERE   cm.id=:cmid";
-    $DB->execute($sql, array('cmid' => $mod->cmid));
-    
-    // something might have change, so clear cache
-    rebuild_course_cache($mod->courseid, true);
-}
-
-/**
  * Deletes a given user's repo keys or all users whose lastaccess is passed a 
  * given time interval.
  * 
@@ -66,16 +40,6 @@ function delete_repo_keys($param = null) {
     }
     
     return true;
-}
-
-/**
- * Created because cannot declare more than once function handler per event.
- * 
- * @param object $eventdata
- */
-function local_ucla_handle_mod($eventdata) {
-    check_mod_parent_visiblity($eventdata);
-    delete_repo_keys($eventdata);
 }
 
 function ucla_sync_built_courses($edata) {
