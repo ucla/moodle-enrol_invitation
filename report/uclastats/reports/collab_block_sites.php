39f85d5693e2f210ec3f87d7df718098c455b765
Anthony Su <asu@oid.ucla.edu>
2013-02-07 12:06:01 -0800

    CCLE-3734     1. updated formatting of tables to include curly braces     2. removed param term from collab block sites since param is not used in query

--- report/uclastats/reports/collab_block_sites.php
+++ report/uclastats/reports/collab_block_sites.php
@@ -36,7 +36,7 @@ class collab_block_sites extends uclastats_base {
      * Returns an array of form elements used to run report.
      */
     public function get_parameters() {
-        return array('term');
+        return array();
     }
 
     /**
@@ -49,29 +49,25 @@ class collab_block_sites extends uclastats_base {
         global $DB;
 
         $sql = "SELECT bi.blockname,COUNT(bi.id) as collab_block_count
-                FROM mdl_course c
-                JOIN 
-                mdl_context ct ON (
-                 ct.contextlevel = 50 AND
-                 ct.instanceid = c.id)
-                 JOIN
-                 mdl_block_instances bi ON (
-                 bi.parentcontextid = ct.id
-                 )
-                 JOIN
-                 mdl_block b ON (
-                 bi.blockname = b.name
-                 )
-                 LEFT JOIN mdl_ucla_siteindicator AS si ON ( c.id = si.courseid )
-                 WHERE
-                 c.id NOT IN (
-                 SELECT courseid
-                 FROM mdl_ucla_request_classes
+                FROM {course} c
+                JOIN {context} ct ON (
+                    ct.contextlevel = 50 AND
+                    ct.instanceid = c.id)
+                JOIN {block_instances} bi ON (
+                    bi.parentcontextid = ct.id
+                )
+                JOIN {block} b ON (
+                    bi.blockname = b.name
+                )
+                LEFT JOIN {ucla_siteindicator} AS si ON ( c.id = si.courseid )
+                WHERE c.id NOT IN (
+                    SELECT courseid
+                    FROM {ucla_request_classes}
                  ) AND
-                 si.type!='test'
-                 GROUP BY b.id
-                 ORDER BY b.name";
-        return $DB->get_records_sql($sql, array('shortname' => $params['term'] . "-%"));
+                si.type!='test'
+                GROUP BY b.id
+                ORDER BY b.name";
+        return $DB->get_records_sql($sql, $params);
     }
 
 }
--- report/uclastats/reports/course_block_sites.php
+++ report/uclastats/reports/course_block_sites.php
@@ -49,27 +49,20 @@ class course_block_sites extends uclastats_base {
     public function query($params) {
         global $DB;
 
-        $sql = "SELECT
-               bi.blockname,
-               COUNT(bi.id) as course_block_count
-               FROM
-               mdl_course c
-               JOIN
-               mdl_context ct ON (
-               ct.contextlevel = 50 AND
-               ct.instanceid = c.id)
-               JOIN
-               mdl_block_instances bi ON (
-               bi.parentcontextid = ct.id
-               )
-               JOIN
-               mdl_block b ON (
-               bi.blockname = b.name
-               )
-               WHERE
-               c.shortname LIKE :shortname
-               GROUP BY b.id
-               ORDER BY b.name";
+        $sql = "SELECT bi.blockname, COUNT(bi.id) as course_block_count
+                FROM {course} c
+                JOIN {context} ct ON (
+                    ct.contextlevel = 50 AND
+                    ct.instanceid = c.id)
+                JOIN {block_instances} bi ON (
+                    bi.parentcontextid = ct.id
+                )
+                JOIN {block} b ON (
+                    bi.blockname = b.name
+                )
+                WHERE c.shortname LIKE :shortname
+                GROUP BY b.id
+                ORDER BY b.name";
         return $DB->get_records_sql($sql, array('shortname' => $params['term'] . "-%"));
     }
 
