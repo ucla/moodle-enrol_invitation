45f1a40f9730f3b7128b53f10f468ea821b32c93
yufeichen <yufeichen@erdos>
2013-02-26 16:08:16 -0800

    CCLE-3703 Move lang strings out of core moodle.php * Moved lang strings references

--- group/grouping.php
+++ group/grouping.php
@@ -89,7 +89,7 @@ if ($id and $delete) {
          */
         if($publicprivate_course->is_grouping($grouping)) {
             echo $OUTPUT->heading($course->fullname. ': '. get_string('deletegrouping', 'group'));
-            echo $OUTPUT->notification(get_string('publicprivatecannotremove'));
+            echo $OUTPUT->notification(get_string('publicprivatecannotremove','local_publicprivate'));
             echo $OUTPUT->continue_button('groupings.php?id='.$courseid);
         } else {
             $optionsyes = array('id'=>$id, 'delete'=>1, 'courseid'=>$courseid, 'sesskey'=>sesskey(), 'confirm'=>1);
@@ -169,7 +169,7 @@ echo $OUTPUT->heading($strheading);
  * @version 20110719
  */
 if($publicprivate_course->is_grouping($grouping)) {
-    echo $OUTPUT->notification(get_string('publicprivatecannotremove'));
+    echo $OUTPUT->notification(get_string('publicprivatecannotremove','local_publicprivate'));
     echo $OUTPUT->continue_button('groupings.php?id='.$courseid);
     echo $OUTPUT->footer();
     die;
