e11fa815cb2dc948e32223eb04d9f11cd79da27f
Anthony Su <asu@oid.ucla.edu>
2013-04-25 15:28:58 -0700

    CCLE-3732 * added upgrade.php to rename file_size reports to system_size_reports           * updated plugin version of uclastats

---
+++ report/uclastats/db/upgrade.php
@@ -0,0 +1,23 @@
+<?php 
+
+function xmldb_report_uclastats_upgrade($oldversion = 0) {
+    global $DB;
+
+    $result = true;
+    
+   
+    if ($result && $oldversion < 2013042500) {
+    
+    //update existing cached results such that
+    //file size reports are renamed to system_size reports
+    //Note: used execute to avoid overhead of locating all records
+    //and then deleting them
+    //only use execute if there are no variable params to avoid sql injectionS
+    $DB->execute("UPDATE mdl_ucla_statsconsole 
+                  SET name = 'system_size' 
+                  WHERE name = 'file_size'");
+        
+    }
+
+    return $result;
+}
