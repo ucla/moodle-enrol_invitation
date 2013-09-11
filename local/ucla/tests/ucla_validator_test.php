e20d4de97b4cb765094429c6d5407cf39363f272
Rex Lorenzo <rex@oid.ucla.edu>
2013-09-10 19:08:38 -0700

    CCLE-4098 - [PHPUnit] ucla_validator_test.test_exceptions
    * Updated to conform to Moodle coding standards.

--- local/ucla/tests/ucla_validator_test.php
+++ local/ucla/tests/ucla_validator_test.php
@@ -1,71 +1,98 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
  * Unit tests for ucla_validator.
+ *
+ * @package    local_ucla
+ * @subpackage phpunit
+ * @copyright  2013 UC Regents
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
- 
-if (!defined('MOODLE_INTERNAL')) {
-    die('Direct access to this script is forbidden.'); //  It must be included from a Moodle page
-}
- 
+
+defined('MOODLE_INTERNAL') || die();
+
 global $CFG;
+require_once($CFG->dirroot . '/local/ucla/lib.php');
 
-// Make sure the code being tested is accessible.
-require_once($CFG->dirroot . '/local/ucla/lib.php'); // Include the code to test
- 
+/**
+ * UCLA validator test cases.
+ * 
+ * @package    local_ucla
+ * @subpackage phpunit
+ * @copyright  2013 UC Regents
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 class ucla_validator_test extends basic_testcase {
-    function test_valid_inputs() {
-        $result = ucla_validator('term','11F');
+
+    /**
+     * Test valid inputs.
+     */
+    public function test_valid_inputs() {
+        $result = ucla_validator('term', '11F');
         $this->assertEquals($result, true);
-        $result = ucla_validator('term','11W');
-        $this->assertEquals($result, true);         
-        $result = ucla_validator('term','11S');
-        $this->assertEquals($result, true); 
-        $result = ucla_validator('term','111');
+        $result = ucla_validator('term', '11W');
         $this->assertEquals($result, true);
-        $result = ucla_validator('term','00S');
+        $result = ucla_validator('term', '11S');
         $this->assertEquals($result, true);
-        $result = ucla_validator('srs','111111111');
+        $result = ucla_validator('term', '111');
         $this->assertEquals($result, true);
-        $result = ucla_validator('srs','000000000');
+        $result = ucla_validator('term', '00S');
         $this->assertEquals($result, true);
-        $result = ucla_validator('uid','000000000');
+        $result = ucla_validator('srs', '111111111');
         $this->assertEquals($result, true);
-        $result = ucla_validator('uid','123456789');
+        $result = ucla_validator('srs', '000000000');
+        $this->assertEquals($result, true);
+        $result = ucla_validator('uid', '000000000');
+        $this->assertEquals($result, true);
+        $result = ucla_validator('uid', '123456789');
         $this->assertEquals($result, true);
     }
-    
-    function test_invalid_inputs() {
-        $result = ucla_validator('srs','00000000');
+
+    /**
+     * Test invalid inputs.
+     */
+    public function test_invalid_inputs() {
+        $result = ucla_validator('srs', '00000000');
         $this->assertEquals($result, false);
-        $result = ucla_validator('srs','0000000011');
+        $result = ucla_validator('srs', '0000000011');
         $this->assertEquals($result, false);
-        $result = ucla_validator('uid','00000000');
+        $result = ucla_validator('uid', '00000000');
         $this->assertEquals($result, false);
-        $result = ucla_validator('uid','0000000011');
+        $result = ucla_validator('uid', '0000000011');
         $this->assertEquals($result, false);
-        $result = ucla_validator('term','1111');
+        $result = ucla_validator('term', '1111');
         $this->assertEquals($result, false);
-        $result = ucla_validator('term','110');
+        $result = ucla_validator('term', '110');
         $this->assertEquals($result, false);
-        $result = ucla_validator('term','FF0');
+        $result = ucla_validator('term', 'FF0');
         $this->assertEquals($result, false);
-        $result = ucla_validator('term','1F0');
+        $result = ucla_validator('term', '1F0');
         $this->assertEquals($result, false);
-        $result = ucla_validator('term','11FF');
+        $result = ucla_validator('term', '11FF');
         $this->assertEquals($result, false);
     }
-    
-    function test_exceptions() {
-        try{
-            $result = ucla_validator('ter','1F0');
-            
-        }
-        catch (Exception $e){
-            $this->assertEquals($e->getMessage(), 'ucla_validator/invalid type');
-        }        
+
+    /**
+     * Makes sure that an invalid validator type will throw an exception.
+     *
+     * @expectedException moodle_exception
+     */
+    public function test_exceptions() {
+        ucla_validator('ter', '1F0');
     }
- 
+
 }
-
-
-//EOF
