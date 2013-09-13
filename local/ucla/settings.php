810280d76735a6a1a599a40b4942cd698a635a87
Rex Lorenzo <rex@oid.ucla.edu>
2013-09-13 16:00:55 -0700

    CCLE-3970 - Install and evaluate LSU's Gradebook Improvements
    * Preventing crash when PHPunit runs its init process or non-logged in user tries to view admin page.

--- local/ucla/settings.php
+++ local/ucla/settings.php
@@ -56,12 +56,15 @@ if ($hassiteconfig) {
 // Inject new gradebook settings for repeat headers and non-override category totals.
 if ($ADMIN->fulltree) {
     $grade = $ADMIN->locate('gradecategorysettings');
-    $grade->add(new admin_setting_configcheckbox('grade_overridecat',
-            get_string('overridecat', 'local_ucla'),
-            get_string('overridecat_help', 'local_ucla'), 1));
-
+    if (!empty($grade)) {
+        $grade->add(new admin_setting_configcheckbox('grade_overridecat',
+                get_string('overridecat', 'local_ucla'),
+                get_string('overridecat_help', 'local_ucla'), 1));
+    }
     $grader = $ADMIN->locate('gradereportgrader');
-    $grader->add(new admin_setting_configtext('grade_report_repeatheaders',
-            get_string('repeatheaders', 'local_ucla'),
-            get_string('repeatheaders_help', 'local_ucla'), 10));
+    if (!empty($grader)) {
+        $grader->add(new admin_setting_configtext('grade_report_repeatheaders',
+                get_string('repeatheaders', 'local_ucla'),
+                get_string('repeatheaders_help', 'local_ucla'), 10));
+    }
 }
